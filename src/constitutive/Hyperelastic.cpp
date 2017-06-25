cd3ce30813defcb82a5e350cf1dbc65fdc16a7ec
Darcy Beurle <darcy.beurle@ibnm.uni-hannover.de>
2017-06-25 13:07:06 +0200

    Changed hyperelastic model to force Matrix3 return type

--- src/constitutive/Hyperelastic.cpp
+++ src/constitutive/Hyperelastic.cpp
@@ -9,6 +9,8 @@
 #include <range/v3/algorithm.hpp>
 #include <range/v3/view.hpp>
 
+#include <iostream>
+
 namespace neon
 {
 Hyperelastic::Hyperelastic(InternalVariables& variables) : ConstitutiveModel(variables) {}
@@ -29,23 +31,21 @@ NeoHooke::NeoHooke(InternalVariables& variables, Json::Value const& material_dat
 void NeoHooke::update_internal_variables()
 {
     // Get references into the hash table
-    auto[Fvec, Kvec] = variables(InternalVariables::Tensor::DeformationGradient,
-                                 InternalVariables::Tensor::Kirchhoff);
+    auto[F, k] = variables(InternalVariables::Tensor::DeformationGradient,
+                           InternalVariables::Tensor::Kirchhoff);
 
-    // Perform the update of the internal variables
-    std::transform(Fvec.begin(), Fvec.end(), Kvec.begin(), [this](auto const& F) {
+    k = F | ranges::view::transform([this](auto const& F) -> Matrix3 {
+            auto const[μ0, λ0] = material.LameConstants();
 
-        auto const[μ0, λ0] = material.LameConstants();
+            // Left Cauchy Green deformation tensor
+            auto const B = F * F.transpose();
 
-        // Left Cauchy Green deformation tensor
-        Matrix3 B = F.matrix() * F.matrix().transpose();
+            auto const J = F.determinant();
 
-        auto const J = F.determinant();
+            auto const I = Matrix3::Identity();
 
-        Matrix3 const I = Matrix3::Identity();
-
-        return λ0 * std::log(J) * I + μ0 * (B - I);
-    });
+            return λ0 * std::log(J) * I + μ0 * (B - I);
+        });
 }
 
 void NeoHooke::update_continuum_tangent()
@@ -53,9 +53,7 @@ void NeoHooke::update_continuum_tangent()
     auto& DVec = variables(InternalVariables::Matrix::MaterialTangent);
     auto const& Fvec = variables(InternalVariables::Tensor::DeformationGradient);
 
-    using namespace ranges;
-
-    for_each(view::zip(Fvec, DVec), [&](auto const& zip_pair) {
+    ranges::for_each(ranges::view::zip(Fvec, DVec), [&](auto const& zip_pair) {
 
         auto & [ F, D ] = zip_pair;
 
