c316ac23dedf6edaf837357b2f47a98865abd92f
Darcy Beurle <darcy@beurle.id.au>
2017-07-04 08:34:40 +0200

    Added stress computation for affine microsphere model

--- src/constitutive/Hyperelastic.cpp
+++ src/constitutive/Hyperelastic.cpp
@@ -72,4 +72,76 @@ void NeoHooke::update_continuum_tangent()
             0.0, 0.0, 0.0, 0.0, 0.0, μ;
     });
 }
+
+AffineMicrosphere::AffineMicrosphere(InternalVariables& variables, Json::Value const& material_data)
+    : Hyperelastic(variables), material(material_data)
+{
+    // The Neo-Hookean model requires the following deformation measures
+    // - C, right Cauchy-Green tensor for S, the PK1 stress (total)
+    // - B, left Cauchy-Green tensor for kappa, the Kirchoff stress (updated)
+    // Which can be computed from the deformation gradient and transformed
+    // to the Cauchy stress with very little effort
+    variables.add(InternalVariables::Matrix::MaterialTangent, 6);
+}
+
+void AffineMicrosphere::update_internal_variables()
+{
+    using namespace ranges;
+
+    // Get references into the hash table
+    auto[F, σ] = variables(InternalVariables::Tensor::DeformationGradient,
+                           InternalVariables::Tensor::CauchyStress);
+    auto const& detF = variables(InternalVariables::Scalar::DetF);
+
+    σ = view::zip(F, detF) | view::transform([this](auto const& FdetF) -> Matrix3 {
+
+            auto const[μ0, λ0] = material.LameConstants();
+
+            auto const & [ F, J ] = FdetF;
+
+            // Unimodular decomposition of F
+            Matrix3 const unimodular_F = std::pow(J, -1.0 / 3.0) * F;
+
+            auto const& N = segments_per_chain;
+
+            return μ0 *
+                   unit_sphere.integrate(Matrix3::Zero(),
+                                         [&](auto const& coordinates, auto const& l) -> Matrix3 {
+                                             auto const & [ r, r_outer_r ] = coordinates;
+
+                                             // Deformed tangents
+                                             auto const t = unimodular_F * r;
+
+                                             // Microstretches
+                                             auto const λ = t.norm();
+
+                                             return (3.0 * N - std::pow(λ, 2)) /
+                                                    (N - std::pow(λ, 2)) * t * t.transpose();
+                                         }) /
+                   J;
+        });
+}
+
+void AffineMicrosphere::update_continuum_tangent()
+{
+    auto& D_list = variables(InternalVariables::Matrix::MaterialTangent);
+    auto const& F_list = variables(InternalVariables::Tensor::DeformationGradient);
+    auto const& detF_list = variables(InternalVariables::Scalar::DetF);
+
+    ranges::for_each(ranges::view::zip(F_list, D_list, detF_list), [&](auto const& FDdetF) {
+
+        auto & [ F, D, J ] = FDdetF;
+
+        auto const[μ0, λ0] = material.LameConstants();
+
+        auto const μ = μ0 - λ0 * std::log(J);
+
+        D << λ0 + 2.0 * μ, λ0, λ0, 0.0, 0.0, 0.0, //
+            λ0, λ0 + 2.0 * μ, λ0, 0.0, 0.0, 0.0,  //
+            λ0, λ0, λ0 + 2.0 * μ, 0.0, 0.0, 0.0,  //
+            0.0, 0.0, 0.0, μ, 0.0, 0.0,           //
+            0.0, 0.0, 0.0, 0.0, μ, 0.0,           //
+            0.0, 0.0, 0.0, 0.0, 0.0, μ;
+    });
+}
 }
--- src/constitutive/Hyperelastic.hpp
+++ src/constitutive/Hyperelastic.hpp
@@ -6,6 +6,7 @@
 #include <json/forwards.h>
 
 #include "material/LinearElastic.hpp"
+#include "quadrature/UnitSphereQuadrature.hpp"
 
 namespace neon
 {
@@ -30,5 +31,26 @@ public:
 
 private:
     LinearElastic material; //!< Elastic model where C1 = mu/2 and C2 = bulk-modulus / 2
+};
+
+class AffineMicrosphere : public Hyperelastic
+{
+public:
+    /**
+     * @param variables Reference to internal state variable store
+     * @param data Json object with material data
+     */
+    explicit AffineMicrosphere(InternalVariables& variables, Json::Value const& material_data);
+
+    void update_internal_variables() override final;
+
+    void update_continuum_tangent() override final;
+
+protected:
+    LinearElastic material; //!< Elastic model where C1 = mu/2 and C2 = bulk-modulus / 2
+
+    UnitSphereQuadrature unit_sphere;
+
+    double segments_per_chain = 0.0;
 };
 }
