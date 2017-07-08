ff290627e380fb506c17e2960c8e70bbec353d87
Darcy Beurle <darcy@neonfe.io>
2017-07-08 12:54:30 +0200

    Time parameter handling for time integrators

---
+++ src/solver/TimeControl.cpp
@@ -0,0 +1,26 @@
+
+#include "TimeControl.hpp"
+
+#include <exception>
+#include <json/value.h>
+
+namespace neon
+{
+TimeControl::TimeControl(Json::Value const& time_data)
+{
+    if (time_data["Start"].empty())
+        throw std::runtime_error("Start time not specified in input (\"Start\")\n");
+
+    if (time_data["End"].empty())
+        throw std::runtime_error("End time not specified in input (\"End\")\n");
+
+    if (time_data["StepSize"].empty())
+        throw std::runtime_error("\"StepSize\" not specified in input\n");
+
+    double total_time = time_data["End"].asDouble() - time_data["Start"].asDouble();
+
+    time_step_size = time_data["StepSize"].asDouble();
+
+    time_steps = static_cast<int>(total_time / time_step_size);
+}
+}
---
+++ src/solver/TimeControl.hpp
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include <json/forwards.h>
+
+namespace neon
+{
+class TimeControl
+{
+public:
+    /** Constructor for static solution */
+    TimeControl() = default;
+
+    /** Constructor for dynamic solution */
+    TimeControl(Json::Value const& time_data);
+
+    double current_time_step_size() const { return time_step_size; }
+
+    int number_of_time_steps() const { return time_steps; }
+
+    void increment() { current_time_step++; }
+
+    bool is_finished() const { return current_time_step == time_steps; }
+
+protected:
+    int time_steps = 1, current_time_step = 0;
+    double time_step_size = 1.0;
+};
+}
