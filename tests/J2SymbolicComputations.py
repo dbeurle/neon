c8a82391daef3504347593bac14413502c3c7f67
Darcy Beurle <darcy@beurle.id.au>
2017-07-09 23:18:02 +0200

    Extended model and fixed bug

--- tests/J2SymbolicComputations.py
+++ tests/J2SymbolicComputations.py
@@ -34,13 +34,13 @@ for a in range(0, 3):
     i, j = voigt_map[a]
     for b in range(0, 3):
         k, l = voigt_map[b]
-        
+
         δik = float(i == k)
         δjl = float(j == l)
         δil = float(i == l)
         δjk = float(j == k)
-        
-        Cdash[a, b] = 0.5 * (δik*σ[j,l] + δil*σ[j,k] + δjk*σ[i,l] + δjl*σ[i,k])
+
+        Cdash[a, b] = Rational(1, 2) * (δik*σ[j,l] + δil*σ[j,k] + δjk*σ[i,l] + δjl*σ[i,k])
 
 print('Plane strain case\n')
 pprint(Cdash)
@@ -61,16 +61,16 @@ for a in range(0, 6):
     i, j = voigt_map[a]
     for b in range(0, 6):
         k, l = voigt_map[b]
-        
-        δik = float(i == k)
-        δjl = float(j == l)
-        δil = float(i == l)
-        δjk = float(j == k)
-        
-        Cdash[a, b] = 0.5 * (δik*σ[j,l] + δil*σ[j,k] + δjk*σ[i,l] + δjl*σ[i,k])
+
+        δik = S(int(i == k))
+        δjl = S(int(j == l))
+        δil = S(int(i == l))
+        δjk = S(int(j == k))
+
+        Cdash[a, b] = Rational(1, 2) * (δik*σ[j,l] + δil*σ[j,k] + δjk*σ[i,l] + δjl*σ[i,k])
 
 print('\nContinuum case\n')
-pprint(Cdash)
+pprint(simplify(Cdash))
 
 #---------------------------------------------------------#
 #                          I_hat                          #
@@ -81,22 +81,47 @@ print('\nCompute Ihat for consistent modulus\n')
 n = MatrixSymbol('n', 3, 3)
 
 Ihat = zeros(6, 6)
+I_outer_I = zeros(6, 6)
+I_dev = zeros(6, 6)
+n_outer_n = zeros(6, 6)
+I4 = zeros(6, 6)
+
+C_e = zeros(6, 6)
+
+λ, gamma, beta, mu = symbols('λ, gamma, beta, mu')
 
 for i in range(0, 6):
     a, b = voigt_map[i]
     for j in range(0, 6):
         c, d = voigt_map[j]
 
-        δab = float(a == b)
-        δac = float(a == c)
-        δad = float(a == d)
-        δbc = float(b == c)
-        δcd = float(c == d)
-        δbd = float(b == d)
-        
-        # Ihat = I - 1/3 * 1-outer-1
-        Ihat[i, j] = 0.5 * ( δac*δbd + δac*δbc ) - 1.0 / 3.0 * δab * δcd - n[a, b] * n[c, d]
+        δab = S(int(a == b))
+        δac = S(int(a == c))
+        δad = S(int(a == d))
+        δbc = S(int(b == c))
+        δcd = S(int(c == d))
+        δbd = S(int(b == d))
+
+        I_outer_I[i, j] = δab * δcd
+        n_outer_n[i, j] = n[a, b] * n[c, d]
+
+        I4[i, j] = Rational(1, 2) * ( δac*δbd + δad*δbc )
+
+        I_dev[i, j] = I4[i, j] - Rational(1, 3) * I_outer_I[i, j]
+
+        # Continuum mechanics tensors
+
+        Ihat[i, j] = I_dev[i, j] - n_outer_n[i, j]
+
+        C_e[i, j] = λ * I_outer_I[i, j] + 2 * mu * I4[i, j]
 
 pprint(Ihat)
 
+#--------------------------------------------------------------#
+#               Algorithmically consistent modulus             #
+#--------------------------------------------------------------#
 
+
+Calg = (λ +  Rational(2, 3) * mu) * I_outer_I + 2 * mu * beta * I_dev # - 2 * mu * (gamma - 1 + beta) * n_outer_n
+
+pprint(factor(Calg))
