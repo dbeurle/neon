3386ba6d526c1ddb0d60ba82797314d6938ee737
Darcy Beurle <darcy@beurle.id.au>
2017-07-11 08:22:25 +0200

    Enforced return types and added deformation gradient computation

--- src/numeric/Tensor.hpp
+++ src/numeric/Tensor.hpp
@@ -5,19 +5,50 @@
 
 namespace neon
 {
-inline auto double_dot(Matrix3 const& a, Matrix3 const& b) { return (a.array() * b.array()).sum(); }
+inline double double_dot(Matrix3 const& a, Matrix3 const& b)
+{
+    return (a.array() * b.array()).sum();
+}
 
 /** @return the deviatoric part of the tensor */
-inline auto deviatoric(Matrix3 const& a) { return a - Matrix3::Identity() * a.trace() / 3.0; }
+inline Matrix3 deviatoric(Matrix3 const& a) { return a - Matrix3::Identity() * a.trace() / 3.0; }
 
+/** Compute the von Mises stress based on the full stress tensor */
 inline double von_mises_stress(Matrix3 const& a)
 {
     return std::sqrt(3.0 / 2.0) * deviatoric(a).norm();
 }
 
-inline auto symmetric(Matrix3 const& a) { return 1.0 / 2.0 * (a.transpose() + a); }
+inline Matrix3 symmetric(Matrix3 const& a) { return 0.5 * (a.transpose() + a); }
 
-inline auto voigt(Matrix3 const& a)
+/**
+ * Compute the time derivative of the deformation gradient from the current
+ * and the last deformation gradient and the increment between them
+ */
+inline Matrix3 time_derivative(Matrix3 const& F, Matrix3 const& F_old, double const Δt)
+{
+    return (F - F_old) / Δt;
+}
+
+/**
+ * Compute the velocity gradient given the time derivative of the deformation
+ * gradient and the deformation gradient
+ */
+inline Matrix3 velocity_gradient(Matrix3 const& Fdot, Matrix const& F)
+{
+    return Fdot * F.inverse();
+}
+
+/** Compute the rate of deformation given the velocity gradient */
+inline Matrix3 rate_of_deformation(Matrix3 const& L) { return symmetric(L); }
+
+/** Compute the rate of deformation given the velocity gradient */
+inline Matrix3 rate_of_deformation(Matrix3 const& F, Matrix3 const& F_old, double const Δt)
+{
+    return symmetric(velocity_gradient(time_derivative(F, F_old, Δt), F));
+}
+
+inline Eigen::Matrix<double, 6, 1> voigt(Matrix3 const& a)
 {
     Eigen::Matrix<double, 6, 1> b;
     b << a(0, 0), a(1, 1), a(2, 2), a(1, 2), a(0, 2), a(0, 1);
@@ -38,7 +69,7 @@ inline Matrix3 voigt_to_matrix(Vector const& a)
  * which is equal to the trace
  * @return First invariant
  */
-inline auto I1(Matrix3 const& a) { return a.trace(); }
+inline double I1(Matrix3 const& a) { return a.trace(); }
 
 /**
  * I2 returns the coefficient I2, the second stress invariant,
@@ -48,12 +79,12 @@ inline auto I1(Matrix3 const& a) { return a.trace(); }
  * \f}
  * @return Second invariant
  */
-inline auto I2(Matrix3 const& a) { return 0.5 * (std::pow(a.trace(), 2) - (a * a).trace()); }
+inline double I2(Matrix3 const& a) { return 0.5 * (std::pow(a.trace(), 2) - (a * a).trace()); }
 
 /** @return Third invariant, which is the determinant of the tensor */
-inline auto I3(Matrix3 const& a) { return a.determinant(); }
+inline double I3(Matrix3 const& a) { return a.determinant(); }
 
-inline auto identity_expansion(Matrix const& H, int const nodal_dofs)
+inline Matrix identity_expansion(Matrix const& H, int const nodal_dofs)
 {
     assert(H.rows() == H.cols());
     // Create the geometric part of the tangent stiffness matrix
@@ -65,7 +96,7 @@ inline auto identity_expansion(Matrix const& H, int const nodal_dofs)
     return K;
 }
 
-inline auto fourth_order_identity()
+inline Matrix fourth_order_identity()
 {
     Matrix I(6, 6);
     I << 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, //
@@ -77,7 +108,7 @@ inline auto fourth_order_identity()
     return I;
 }
 
-inline auto I_outer_I()
+inline Matrix I_outer_I()
 {
     Matrix i_o_i(6, 6);
     i_o_i << 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, //
@@ -89,7 +120,7 @@ inline auto I_outer_I()
     return i_o_i;
 }
 
-inline auto outer_product(Matrix3 const& h)
+inline Matrix outer_product(Matrix3 const& h)
 {
     Matrix h_outer_h(6, 6);
     h_outer_h << std::pow(h(0, 0), 2), //
