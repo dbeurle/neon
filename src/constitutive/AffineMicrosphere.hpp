d2de4cdcb5364a774104e9751c1c78d206840c07
Darcy Beurle <darcy@neonfe.io>
2017-07-17 11:31:47 +0200

    Documentation about free energy function used

--- src/constitutive/AffineMicrosphere.hpp
+++ src/constitutive/AffineMicrosphere.hpp
@@ -18,18 +18,49 @@ public:
      * @param variables Reference to internal state variable store
      * @param data Json object with material data
      */
-    explicit AffineMicrosphere(InternalVariables& variables, Json::Value const& material_data);
+    explicit AffineMicrosphere(InternalVariables& variables,
+                               Json::Value const& material_data);
 
     virtual void update_internal_variables(double const Δt) override;
 
     Material const& intrinsic_material() const override final { return material; };
 
 protected:
-    double volumetric_free_energy_derivative(double const J, double const bulk_modulus) const;
-    double volumetric_free_energy_second_derivative(double const J, double const bulk_modulus) const;
+    /**
+     * \f{align*}{
+         U' &= \frac{\partial U}{\partial J} = \frac{K}{2}\left(J - \frac{1}{J}\right)
+       \f}
+     * where
+     * \f{align*}{
+         U &= \frac{K}{4}(J^2 - 1) - \frac{K}{2}\ln{J}
+       \f}
+     */
+    double volumetric_free_energy_derivative(double const J,
+                                             double const bulk_modulus) const;
 
+    /**
+     * \f{align*}{
+         U'' &= \frac{\partial^2 U}{\partial J^2} = \frac{K}{2} \left(1 +
+     \frac{1}{J^2}\right) \f}
+     * \sa volumetric_free_energy_derivative
+     */
+    double volumetric_free_energy_second_derivative(double const J,
+                                                    double const bulk_modulus) const;
+
+    /**
+     *\f{align*}{
+     * \boldsymbol{\tau} &= p \boldsymbol{g}^{-1} + \mathbb{P} : \bar{\boldsymbol{\tau}}
+     * \f}
+     */
     Matrix3 deviatoric_projection(double const pressure, Matrix3 const& τ_dev) const;
 
+    /**
+     *\f{align*}{
+        \boldsymbol{C} &= \mathbb{P} : \left[\bar{\mathbb{C}} +
+     \frac{2}{3}(\boldsymbol{\tau} : \boldsymbol{g}) \mathbb{I} - \frac{2}{3}
+     (\bar{\boldsymbol{\tau}} \otimes \boldsymbol{g}^{-1} + \boldsymbol{g}^{-1} \otimes
+     \bar{\boldsymbol{\tau}}) \right] : \mathbb{P} \f}
+     */
     Matrix deviatoric_projection(Matrix const& C_dev, Matrix3 const& τ_dev) const;
 
 protected:
@@ -41,8 +72,8 @@ protected:
     Matrix const I = fourth_order_identity();
 
     double number_of_chains;
-    double const boltzmann_constant = 1.38064852e-23;
-    double const temperature = 298.0;
+    double constexpr boltzmann_constant = 1.38064852e-23;
+    double constexpr temperature = 298.0;
 
     double μ; //!< Shear modulus
 
@@ -56,8 +87,9 @@ inline double AffineMicrosphere::volumetric_free_energy_derivative(double const 
 {
     return bulk_modulus / 2.0 * (J - 1.0 / J);
 }
-inline double AffineMicrosphere::volumetric_free_energy_second_derivative(double const J,
-                                                                          double const bulk_modulus) const
+inline double AffineMicrosphere::volumetric_free_energy_second_derivative(
+    double const J,
+    double const bulk_modulus) const
 {
     return bulk_modulus / 2.0 * (1.0 + 1.0 / std::pow(J, 2));
 }
