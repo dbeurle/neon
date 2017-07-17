29ca070e85eee53ddf4cb727e251a31e6f3834e5
Darcy Beurle <darcy@neonfe.io>
2017-07-17 13:49:39 +0200

    Can now specify a rubber materials with bulk and shear modulus

--- src/material/LinearElastic.cpp
+++ src/material/LinearElastic.cpp
@@ -9,11 +9,29 @@ namespace neon
 {
 LinearElastic::LinearElastic(Json::Value const& material_data) : Material(material_data)
 {
-    if (material_data["ElasticModulus"].empty()) throw MaterialPropertyException("Elastic Modulus");
-    if (material_data["PoissonsRatio"].empty()) throw MaterialPropertyException("Poisson's Ratio");
+    // Determine input value types.  The allowable inputs are:
+    //   1)  Elastic modulus and Poisson ratio
+    //   2)  Bulk modulus and shear modulus
 
-    // Read in from Json file
-    E = material_data["ElasticModulus"].asDouble();
-    nu = material_data["PoissonsRatio"].asDouble();
-}
+    if (material_data.isMember("BulkModulus") && material_data.isMember("ShearModulus"))
+    {
+        // Fill the elastic modulus and Poisson's ratio from the bulk and shear
+        // moduli
+        auto const K = material_data["BulkModulus"].asDouble();
+        auto const G = material_data["ShearModulus"].asDouble();
+
+        E = 9 * K * G / (3 * K + G);
+        nu = (3 * K - 2 * G) / (3 * K + G);
+    }
+    else if (material_data.isMember("ElasticModulus") &&
+             material_data.isMember("PoissonsRatio"))
+    {
+        E = material_data["ElasticModulus"].asDouble();
+        nu = material_data["PoissonsRatio"].asDouble();
+    }
+    else
+    {
+        throw MaterialPropertyException("\"ElasticModulus\" and \"PoissonsRatio\" or "
+                                        "\"BulkModulus\" and \"ShearModulus\"");
+    }
 }
