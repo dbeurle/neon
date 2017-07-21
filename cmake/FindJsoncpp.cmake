d9d57d3933f51ef693c6ea5009305953a7e68709
Darcy Beurle <darcy@beurle.id.au>
2017-07-21 16:07:51 +0200

    Fixed a failure on Ubuntu systems

--- cmake/FindJsoncpp.cmake
+++ cmake/FindJsoncpp.cmake
@@ -1,21 +1,21 @@
 
 #Copyright (C) 2011-2016 Peter Spiess-Knafl
 
-#Permission is hereby granted, free of charge, to any person obtaining a copy of 
-#this software and associated documentation files (the "Software"), to deal in the 
-#Software without restriction, including without limitation the rights to 
-#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
-#the Software, and to permit persons to whom the Software is furnished to do so, 
+#Permission is hereby granted, free of charge, to any person obtaining a copy of
+#this software and associated documentation files (the "Software"), to deal in the
+#Software without restriction, including without limitation the rights to
+#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+#the Software, and to permit persons to whom the Software is furnished to do so,
 #subject to the following conditions:
 #
-#The above copyright notice and this permission notice shall be included in all 
+#The above copyright notice and this permission notice shall be included in all
 #copies or substantial portions of the Software.
 #
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-#INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-#PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
-#LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-#TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+#INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+#PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+#LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 #OR OTHER DEALINGS IN THE SOFTWARE.
 
 # Taken from Cinemast, from user debris.
@@ -23,9 +23,9 @@
 # Find jsoncpp
 #
 # Find the jsoncpp includes and library
-# 
-# if you nee to add a custom library search path, do it via via CMAKE_PREFIX_PATH 
-# 
+#
+# if you nee to add a custom library search path, do it via via CMAKE_PREFIX_PATH
+#
 # This module defines
 #  JSONCPP_INCLUDE_DIRS, where to find header, etc.
 #  JSONCPP_LIBRARIES, the libraries needed to use jsoncpp.
@@ -34,7 +34,7 @@
 
 # only look in default directories
 find_path(
-	JSONCPP_INCLUDE_DIR 
+	JSONCPP_INCLUDE_DIR
 	NAMES jsoncpp/json/json.h json/json.h
 	DOC "jsoncpp include dir"
 )
@@ -57,7 +57,7 @@ if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 		NAMES jsoncppd
 		DOC "jsoncpp debug library"
 	)
-	
+
 	set(JSONCPP_LIBRARIES optimized ${JSONCPP_LIBRARIES} debug ${JSONCPP_LIBRARY_DEBUG})
 
 endif()
@@ -70,9 +70,9 @@ find_path(
 )
 
 if (${JSONCPP_INCLUDE_PREFIX} MATCHES "jsoncpp")
-	set(JSONCPP_INCLUDE_PREFIX "jsoncpp/json")
+	set(JSONCPP_INCLUDE_PREFIX "jsoncpp/")
 else()
-	set(JSONCPP_INCLUDE_PREFIX "json")
+	set(JSONCPP_INCLUDE_PREFIX "")
 endif()
 
 # handle the QUIETLY and REQUIRED arguments and set JSONCPP_FOUND to TRUE
