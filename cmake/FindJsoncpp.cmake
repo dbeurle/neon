f46f4da67c05689974db472f3b82c747329c8233
Darcy Beurle <darcy@beurle.id.au>
2017-07-21 16:59:45 +0200

    Changes to json to find under jsoncpp/json and json

--- cmake/FindJsoncpp.cmake
+++ cmake/FindJsoncpp.cmake
@@ -32,52 +32,32 @@
 #  JSONCPP_FOUND, If false, do not try to use jsoncpp.
 #  JSONCPP_INCLUDE_PREFIX, include prefix for jsoncpp
 
-# only look in default directories
-find_path(
-	JSONCPP_INCLUDE_DIR
-	NAMES jsoncpp/json/json.h json/json.h
-	DOC "jsoncpp include dir"
-)
+# Only look in default directories
+find_path(JSONCPP_INCLUDE_DIRS
+	      json/json.h
+          PATH_SUFFIXES jsoncpp)
 
-find_library(
-	JSONCPP_LIBRARY
-	NAMES jsoncpp
-	DOC "jsoncpp library"
-)
+find_library(JSONCPP_LIBRARY
+	         NAMES jsoncpp
+	         DOC "jsoncpp library")
 
-set(JSONCPP_INCLUDE_DIRS ${JSONCPP_INCLUDE_DIR})
 set(JSONCPP_LIBRARIES ${JSONCPP_LIBRARY})
-
-# debug library on windows
-# same naming convention as in qt (appending debug library with d)
-# boost is using the same "hack" as us with "optimized" and "debug"
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-	find_library(
-		JSONCPP_LIBRARY_DEBUG
-		NAMES jsoncppd
-		DOC "jsoncpp debug library"
-	)
-
-	set(JSONCPP_LIBRARIES optimized ${JSONCPP_LIBRARIES} debug ${JSONCPP_LIBRARY_DEBUG})
-
-endif()
+set(JSONCPP_INCLUDE_DIRS ${JSONCPP_INCLUDE_DIRS})
 
 # find JSONCPP_INCLUDE_PREFIX
-find_path(
-	JSONCPP_INCLUDE_PREFIX
-	NAMES json.h
-	PATH_SUFFIXES jsoncpp/json json
-)
+find_path(JSONCPP_INCLUDE_PREFIX
+	      json/json.h
+          PATH_SUFFIXES jsoncpp)
 
 if (${JSONCPP_INCLUDE_PREFIX} MATCHES "jsoncpp")
 	set(JSONCPP_INCLUDE_PREFIX "jsoncpp/")
 else()
-	set(JSONCPP_INCLUDE_PREFIX "")
+	set(JSONCPP_INCLUDE_PREFIX "json/")
 endif()
 
 # handle the QUIETLY and REQUIRED arguments and set JSONCPP_FOUND to TRUE
 # if all listed variables are TRUE, hide their existence from configuration view
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(jsoncpp DEFAULT_MSG
-	JSONCPP_INCLUDE_DIR JSONCPP_LIBRARY)
-mark_as_advanced (JSONCPP_INCLUDE_DIR JSONCPP_LIBRARY)
+JSONCPP_INCLUDE_DIRS JSONCPP_LIBRARY)
+mark_as_advanced (JSONCPP_INCLUDE_DIRS JSONCPP_LIBRARY)
