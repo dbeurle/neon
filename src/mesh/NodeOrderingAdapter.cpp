7c13f1f14b44f8019006f73ce540dbb5a03c7a1a
Darcy Beurle <darcy@neonfe.io>
2017-07-28 08:41:07 +0200

    Updated for vtk and tet10

--- src/mesh/NodeOrderingAdapter.cpp
+++ src/mesh/NodeOrderingAdapter.cpp
@@ -19,19 +19,34 @@ void NodeOrderingAdapter::convert_from_gmsh(std::vector<List>& nodal_connectivit
         {
             for (auto& nodal_list : nodal_connectivity)
             {
+                std::swap(nodal_list.at(0), nodal_list.at(3));
                 std::swap(nodal_list.at(4), nodal_list.at(9));
             }
-            break;
-        }
-        case ElementTopology::Triangle6:
-        {
-            std::cout << "Fix the triangle reorder!!!" << std::endl;
-            std::abort();
             break;
         }
         default:
             break;
     }
+}
+
+std::vector<List> NodeOrderingAdapter::convert_to_vtk(std::vector<List> nodal_connectivity,
+                                                      ElementTopology element_topology) const
+{
+    switch (element_topology)
+    {
+        case ElementTopology::Tetrahedron10:
+        {
+            for (auto& nodal_list : nodal_connectivity)
+            {
+                std::swap(nodal_list.at(6), nodal_list.at(8));
+                std::swap(nodal_list.at(8), nodal_list.at(9));
+            }
+            break;
+        }
+        default:
+            break;
+    }
+    return nodal_connectivity;
 }
 
 ElementTopology NodeOrderingAdapter::gmsh_type_to_enum(int element_code) const
--- src/mesh/NodeOrderingAdapter.hpp
+++ src/mesh/NodeOrderingAdapter.hpp
@@ -20,6 +20,9 @@ public:
     /** Mutate the nodalConnectivity */
     void convert_from_gmsh(std::vector<List>& nodal_connectivity, ElementTopology topology);
 
+    std::vector<List> convert_to_vtk(std::vector<List> nodal_connectivity,
+                                     ElementTopology element_topology) const;
+
     /** Add strong types to gmsh integer element codes */
     ElementTopology gmsh_type_to_enum(int const element_code) const;
 
@@ -32,7 +35,8 @@ protected:
                        {4, ElementTopology::Tetrahedron4},
                        {5, ElementTopology::Hexahedron8},
                        {6, ElementTopology::Prism6},
-                       {9, ElementTopology::Triangle6}};
+                       {9, ElementTopology::Triangle6},
+                       {11, ElementTopology::Tetrahedron10}};
 
     std::unordered_map<ElementTopology, int>
         vtk_converter{{ElementTopology::Triangle3, VTK_TRIANGLE},
@@ -40,6 +44,7 @@ protected:
                       {ElementTopology::Tetrahedron4, VTK_TETRA},
                       {ElementTopology::Hexahedron8, VTK_HEXAHEDRON},
                       {ElementTopology::Prism6, VTK_WEDGE},
-                      {ElementTopology::Triangle6, VTK_QUADRATIC_TRIANGLE}};
+                      {ElementTopology::Triangle6, VTK_QUADRATIC_TRIANGLE},
+                      {ElementTopology::Tetrahedron10, VTK_QUADRATIC_TETRA}};
 };
 }
