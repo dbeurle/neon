33a8f1f2f80d1d22f558b89011b5cd77db85c2f4
Darcy Beurle <darcy@neonfe.io>
2017-08-02 18:18:20 +0200

    Added better floating point comparison to load stepper

--- src/numeric/DenseTypes.hpp
+++ src/numeric/DenseTypes.hpp
@@ -24,7 +24,17 @@ using Vector3 = Eigen::Vector3d;
 using Array = Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic>;
 using List = std::vector<int>;
 
-using DofPair = std::pair<bool, double>;
-
 using femValue = std::tuple<Vector, Matrix>;
+
+template <class T>
+typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type is_approx(
+    T const x, T const y, int const ulp = 2)
+{
+    // Taken and modified from
+    // http://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
+    // Since the numeric epsilon is defined at 1.0 then it must be scaled by
+    // the worse case (x + y) and accounted for my the ULP (units in the last place).
+    return std::abs(x - y) < std::numeric_limits<T>::epsilon() * std::abs(x + y) * ulp
+           || std::abs(x - y) < std::numeric_limits<T>::min();
+}
 }
