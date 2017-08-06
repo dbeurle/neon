8e9a6e9b87b41d543581ff897ca169452326c879
Darcy Beurle <darcy@beurle.id.au>
2017-08-06 23:46:52 +0200

    Update the install script for pastix

--- docker-base/install_pastix.sh
+++ docker-base/install_pastix.sh
@@ -7,9 +7,9 @@ ls
 cd pastix_5.2.3
 
 # Build (only can do this once due to some makefile bug)
-patch -p1 < /app/cmake_patch.diff
+patch -p1 < /cmake_patch.diff
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DPASTIX_WITH_MPI=OFF -DPASTIX_INT64=OFF -DBUILD_SHARED_LIBS=ON ../src
-make all && make install
+make all -j4 && make install
 
 ln -s /usr/local/lib/libpastix.so /usr/lib64/libpastix.so
