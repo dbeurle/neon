98bd036b9f0e5d4f387a86f6ac759b66157422f0
Darcy Beurle <darcy@neonfe.io>
2017-10-17 12:07:48 +0200

    Initial work in progress for non-affine model

---
+++ src/constitutive/NonAffineMicrosphere.cpp
@@ -0,0 +1,190 @@
+
+#include "NonAffineMicrosphere.hpp"
+
+#include "InternalVariables.hpp"
+#include "numeric/DenseTypes.hpp"
+
+#include <json/json.h>
+#include <range/v3/algorithm.hpp>
+#include <range/v3/view.hpp>
+
+#include <exception>
+#include <iostream>
+#include <omp.h>
+
+namespace neon
+{
+NonAffineMicrosphere::NonAffineMicrosphere(InternalVariables& variables,
+                                           Json::Value const& material_data)
+    : AffineMicrosphere(variables, material_data)
+{
+    if (!material_data.isMember("NonAffineStretchParameter"))
+    {
+        throw std::runtime_error("\"NonAffineStretchParameter\" not specified in material data\n");
+    }
+    non_affine_stretch_parameter = material_data["NonAffineStretchParameter"].asDouble();
+}
+
+void NonAffineMicrosphere::update_internal_variables(double const time_step_size)
+{
+    using ranges::view::transform;
+    using ranges::view::zip;
+
+    // TODO Change these back once OpenMP allows structured bindings
+
+    // Get references into the hash table
+    auto& F_list = variables(InternalVariables::Tensor::DeformationGradient);
+    auto& cauchy_stress_list = variables(InternalVariables::Tensor::Cauchy);
+    auto& dev_stress_list = variables(InternalVariables::Tensor::Kirchhoff);
+
+    auto const& detF_list = variables(InternalVariables::Scalar::DetF);
+
+    // Material properties
+    auto const K = material.bulk_modulus();
+    auto const G_eff = material.shear_modulus();
+    auto const N = material.segments_per_chain();
+
+    auto const p = non_affine_stretch_parameter;
+
+#pragma omp parallel for
+    for (auto l = 0; l < F_list.size(); ++l)
+    {
+        auto& stress_dev = dev_stress_list[l]; // Deviatoric stress
+        auto const& F = F_list[l];             // Deformation gradient
+        auto const& J = detF_list[l];          // Determinant of the deformation gradient
+
+        Matrix3 const F_deviatoric = std::pow(J, -1.0 / 3.0) * F;
+
+        // Compute the non-affine stretch parts
+        auto const nonaffine_stretch = compute_nonaffine_stretch(F_deviatoric);
+
+        Matrix3 const h = compute_h_tensor(F_deviatoric);
+        Matrix6 const H = compute_H_tensor(F_deviatoric);
+
+        // Compute the microstress and micro moduli
+        auto const micro_kirchhoff_f = G_eff * (3.0 * N - std::pow(nonaffine_stretch, 2))
+                                       / ((N - std::pow(nonaffine_stretch, 2)) * nonaffine_stretch);
+
+        auto const micro_moduli_f = G_eff * (std::pow(nonaffine_stretch, 4) + 3.0 * std::pow(N, 2))
+                                    / std::pow(N - std::pow(nonaffine_stretch, 2), 2);
+
+        // Compute the macrostress and macromoduli for chain force
+        Matrix3 const macro_kirchhoff_f = micro_kirchhoff_f * std::pow(nonaffine_stretch, 1.0 - p)
+                                          * h;
+
+        Matrix6 const macro_moduli_f = (micro_moduli_f * std::pow(nonaffine_stretch, 2.0 - 2.0 * p)
+                                        - (p - 1.0) * micro_kirchhoff_f
+                                              * std::pow(nonaffine_stretch, 1.0 - 2.0 * p))
+                                           * outer_product(h, h)
+                                       + micro_kirchhoff_f * std::pow(nonaffine_stretch, 1.0 - p) * H;
+
+        // Compute the non-affine tube contribution
+        Matrix3 const k = compute_k_tensor(F_deviatoric);
+
+        Matrix6 const K = compute_K_tensor(F_deviatoric);
+        Matrix6 const G = compute_G_tensor(F_deviatoric);
+
+        // Free energy
+        auto const U = AffineMicrosphere::volumetric_free_energy_dJ(J, G_eff);
+
+        // Compute the macrostress and macromoduli for tube contraint
+        Matrix3 const macro_kirchhoff_c = -G_eff * N * U * k;
+
+        Matrix6 const macro_moduli_c = G_eff * N * U * (K + G);
+
+        // Superimposed stress response from tube and chain contributions
+        Matrix3 const macro_kirchhoff = macro_kirchhoff_f + macro_kirchhoff_c;
+
+        Matrix6 const macro_moduli = macro_moduli_f + macro_moduli_c;
+
+        // Perform the deviatoric projection for the stress and moduli
+    }
+}
+
+double NonAffineMicrosphere::compute_nonaffine_stretch(Matrix3 const& F_deviatoric) const
+{
+    auto const p = non_affine_stretch_parameter;
+
+    return std::pow(unit_sphere.integrate(0.0,
+                                          [&](auto const& coordinates, auto const& l) {
+                                              auto const & [ r, r_outer_r ] = coordinates;
+
+                                              // Deformed tangent
+                                              Vector3 const t = F_deviatoric * r;
+
+                                              return std::pow(t.norm(), p);
+                                          }),
+                    1.0 / p);
+}
+
+Matrix3 NonAffineMicrosphere::compute_h_tensor(Matrix3 const& F_deviatoric) const
+{
+    auto const p = non_affine_stretch_parameter;
+
+    return unit_sphere.integrate(Matrix3::Zero().eval(), [&](auto const& xyz, auto const& l) {
+        auto const & [ r, r_o_r ] = xyz;
+
+        Vector3 const t = F_deviatoric * r;
+
+        return std::pow(t.norm(), p - 2.0) * outer_product(t, t);
+    });
+}
+
+Matrix6 NonAffineMicrosphere::compute_H_tensor(Matrix3 const& F_deviatoric) const
+{
+    auto const p = non_affine_stretch_parameter;
+
+    return (p - 2.0)
+           * unit_sphere.integrate(Matrix6::Zero().eval(), [&](auto const& xyz, auto const& l) {
+                 auto const & [ r, r_o_r ] = xyz;
+
+                 Vector3 const t = F_deviatoric * r;
+
+                 return std::pow(t.norm(), p - 4.0) * outer_product(t, t, t, t);
+             });
+}
+
+Matrix3 NonAffineMicrosphere::compute_k_tensor(Matrix3 const& F_deviatoric) const
+{
+    auto const q = non_affine_tube_parameter;
+
+    return unit_sphere.integrate(Matrix3::Zero().eval(), [&](auto const& xyz, auto const& l) {
+        auto const & [ r, r_o_r ] = xyz;
+
+        // Compute deformed normal
+        Vector3 const n = F_deviatoric.inverse().transpose() * r;
+
+        return std::pow(n.norm(), q - 2.0) * outer_product(n, n);
+    });
+}
+
+Matrix6 NonAffineMicrosphere::compute_K_tensor(Matrix3 const& F_deviatoric) const
+{
+    auto const q = non_affine_tube_parameter;
+
+    return q * (q - 2.0)
+           * unit_sphere.integrate(Matrix6::Zero().eval(), [&](auto const& xyz, auto const& l) {
+                 auto const & [ r, r_o_r ] = xyz;
+
+                 // Compute deformed normal
+                 Vector3 const n = F_deviatoric.inverse().transpose() * r;
+
+                 return std::pow(n.norm(), q - 4.0) * outer_product(n, n, n, n);
+             });
+}
+
+Matrix6 NonAffineMicrosphere::compute_G_tensor(Matrix3 const& F_deviatoric) const
+{
+    auto const q = non_affine_tube_parameter;
+
+    return 2.0 * q
+           * unit_sphere.integrate(Matrix6::Zero().eval(), [&](auto const& xyz, auto const& l) {
+                 auto const & [ r, r_o_r ] = xyz;
+
+                 // Compute deformed normal
+                 Vector3 const n = F_deviatoric.inverse().transpose() * r;
+
+                 return std::pow(n.norm(), q - 2.0) * compute_o_dot_product(n);
+             });
+}
+}
---
+++ src/constitutive/NonAffineMicrosphere.hpp
@@ -0,0 +1,117 @@
+
+#pragma once
+
+#include "AffineMicrosphere.hpp"
+
+namespace neon
+{
+/**
+ * NonAffineMicrosphere model computes the Kirchhoff stress and the material
+ * tangent for the non-affine microsphere model by Miehe 2004.
+ */
+class NonAffineMicrosphere : public AffineMicrosphere
+{
+public:
+    /**
+     * @param variables Reference to internal state variable store
+     * @param data Json object with material data
+     */
+    explicit NonAffineMicrosphere(InternalVariables& variables, Json::Value const& material_data);
+
+    virtual void update_internal_variables(double const time_step_size) override;
+
+protected:
+    /**
+     * Compute the non-affine stretch by numerical quadrature over the unit sphere
+     * \f{align*}{
+        \lambda &= \left[ \sum_{i=1}^m (\bar{\lambda}_i)^p w_i \right]^{1/p}
+      \f}
+     */
+    double compute_nonaffine_stretch(Matrix3 const& F_deviatoric) const;
+
+    /**
+     * Evaluate the h tensor by numerical quadrature over the unit sphere
+     * \f{align*}{
+        \mathbf{h} &= \sum_{i=1}^m (\bar{\lambda}_i)^{p - 2}
+                      \mathbf{t}_i\otimes\mathbf{t}_i w_i
+      \f}
+     */
+    Matrix3 compute_h_tensor(Matrix3 const& F_deviatoric) const;
+
+    /**
+     * Evaluate the H tensor by numerical quadrature over the unit sphere
+     * \f{align*}{
+        \mathbf{H} &= (p - 2) \sum_{i=1}^m (\bar{\lambda}_i)^{p-4}
+                      \mathbf{t}_i\otimes\mathbf{t}_i\otimes\mathbf{t}_i\otimes\mathbf{t}_i w_i
+      \f}
+     */
+    Matrix6 compute_H_tensor(Matrix3 const& F_deviatoric) const;
+
+    /**
+     * Evaluate the k tensor by numerical quadrature over the unit sphere
+     * \f{align*}{
+        \mathbf{k} &= q \sum_{i=1}^m (\bar{\nu}_i)^{q-2}
+                      \mathbf{n}_i\otimes\mathbf{n}_i w_i
+      \f}
+     * where \f$ q \f$ is the non-affine tube parameter and \f$ \mathbf{n} \f$
+     * is the deformed normal.
+     * \sa non_affine_tube_parameter
+     * @param F_deviatoric Deviatoric part deformation gradient
+     */
+    Matrix3 compute_k_tensor(Matrix3 const& F_deviatoric) const;
+
+    /**
+     * Evaluate the K tensor by numerical quadrature over the unit sphere
+     * \f{align*}{
+        \mathbf{K} &= q(q-2)\sum_{i=1}^m (\bar{\nu}_i)^{q-4}
+                      \mathbf{n}_i\otimes\mathbf{n}_i\otimes\mathbf{n}_i\otimes\mathbf{n}_iw_i
+      \f}
+     * where \f$ q \f$ is the non-affine tube parameter.
+     * \sa non_affine_tube_parameter
+     */
+    Matrix6 compute_K_tensor(Matrix3 const& F_deviatoric) const;
+
+    /**
+     * Evaluate the G tensor by numerical quadrature over the unit sphere
+     * \f{align*}{
+        \mathbf{G} &= 2q \sum_{i=1}^m (\bar{\nu}_i)^{q - 2}
+        sym[\mathbf{g}^{-1} \odot \mathbf{n} \otimes \mathbf{n} + \mathbf{n} \otimes
+    \mathbf{n} \odot \mathbf{g}^{-1}] w_i
+      \f}
+     * where \f$ q \f$ is the non-affine tube parameter.
+     * \sa non_affine_tube_parameter
+     * \sa compute_o_dot_product
+     */
+    Matrix6 compute_G_tensor(Matrix3 const& F_deviatoric) const;
+
+    /**
+     * Compute the o dot product for the material tangent matrix for the
+     * non-affine model using the formula
+     * \f{align*}{
+         sym[\mathbf{g}^{-1} \odot \mathbf{n} \otimes \mathbf{n} + \mathbf{n} \otimes
+     \mathbf{n} \odot \mathbf{g}^{-1}] & \f}
+     * where \f$\mathbf{g}\f$ is the metric tensor (identity) and \f$\mathbf{n}\f$
+     * is the deformed normal vector
+     * \sa compute_G_tensor
+     */
+    Matrix6 compute_o_dot_product(Vector3 const& n) const;
+
+protected:
+    double non_affine_stretch_parameter{1.0}; //!< Three-dimensional locking characteristics
+    double non_affine_tube_parameter{1.0};    //!< Additional constraint stiffness
+};
+
+inline Matrix6 NonAffineMicrosphere::compute_o_dot_product(Vector3 const& n) const
+{
+    Matrix6 ret_val;
+    // clang-format off
+    ret_val << 2.0 * n(0) * n(0),               0.0,               0.0,                               0.0,                       n(0) * n(2), n(0) * n(1),       //
+                             0.0, 2.0 * n(1) * n(1),               0.0,                       n(1) * n(2),                               0.0, n(0) * n(1),       //
+                             0.0,               0.0, 2.0 * n(2) * n(2),                       n(1) * n(2),                       n(0) * n(2), 0.0,               //
+                             0.0,       n(1) * n(2),       n(1) * n(2), 0.5 * (n(1) * n(1) + n(2) * n(2)),                 0.5 * n(0) * n(1), 0.5 * n(0) * n(2), //
+                     n(0) * n(2),               0.0,       n(0) * n(2),                 0.5 * n(0) * n(1), 0.5 * (n(0) * n(0) + n(2) * n(2)), 0.5 * n(2) * n(1), //
+                     n(0) * n(1),       n(0) * n(1),               0.0,                 0.5 * n(0) * n(2),                 0.5 * n(2) * n(1), 0.5 * (n(0) * n(0) + n(1) * n(1));
+    // clang-format on
+    return ret_val;
+}
+}
