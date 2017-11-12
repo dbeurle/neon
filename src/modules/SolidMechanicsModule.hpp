83079634392d7bd11bae7a6b45e891044191aab6
Darcy Beurle <darcy@beurle.id.au>
2017-11-12 19:30:39 +0100

    Missed from previous commit

--- src/modules/SolidMechanicsModule.hpp
+++ src/modules/SolidMechanicsModule.hpp
@@ -9,10 +9,16 @@
 namespace neon
 {
 //! This namespace groups together all of the classes and functions associated
+//! with solid mechanics finite elements.  The sub-namespaces implement mechanics
+//! such as solid, plate, beam, axisymmetric etc
+namespace mech
+{
+//! This namespace groups together all of the classes and functions associated
 //! with three-dimensional solid mechanics finite elements.  These include
 //! constitutive models, matrix systems, meshes, element stiffness matrices etc.
 namespace solid
 {
+}
 }
 
 /**
@@ -35,7 +41,7 @@ public:
     virtual void perform_simulation() override final { fem_matrix.solve(); }
 
 protected:
-    solid::femMesh fem_mesh;           //!< Mesh with the solid routines
-    solid::femStaticMatrix fem_matrix; //!< Nonlinear solver routines
+    mech::solid::femMesh fem_mesh;           //!< Mesh with the solid routines
+    mech::solid::femStaticMatrix fem_matrix; //!< Nonlinear solver routines
 };
 }
