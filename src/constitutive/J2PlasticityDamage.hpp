8020e93c4c589d8a74e7b6f124f552240bc82838
Shadi Alameddin <shadialameddin@gmail.com>
2017-11-20 00:47:13 +0100

    updated comments
    displacement control
    
    updated comments for the damage model

--- src/constitutive/J2PlasticityDamage.hpp
+++ src/constitutive/J2PlasticityDamage.hpp
@@ -9,6 +9,10 @@
 
 namespace neon::mech::solid
 {
+  /**
+   * J2PlasticityDamage is responsible for computing the constitutive model of a standard ductile Chaboche damage model without microdefects closure effects.
+   * Theoritical details can be found in \cite Neto2011 (Lemaitre’s elastoplastic damage theory), \cite Ladeveze1999 (chapter 2) and \cite Lemaitre2005 (Isotropic Damage without Microdefects Closure).
+   */
 class J2PlasticityDamage : public J2Plasticity
 {
 public:
@@ -17,16 +21,14 @@ public:
     ~J2PlasticityDamage();
 
     /**
-    * Comute the update of the internal variables based on a standard ductile Chaboche damage model
-    [Nonlinear computational structural mechanics, Computational methods for plasticity]
-    * The evolution equations are the following
-              {\dot{\mathbf{\varepsilon}}}^{p} = \dot{\lambda}_{vp} \ \mathbb{\tilde{N}} \
+    * Compute the update of the internal variables based on the following evolution equations.
+    * \f[          {\dot{\mathbf{\varepsilon}}}^{p} = \dot{\lambda}_{vp} \ \mathbf{\tilde{N}} \
     \frac{1}{1-D} \\
-             {\dot{\mathbf{\alpha}}} = \dot{\lambda}_{vp} \ \left( \mathbb{\tilde{N}} -
+             {\dot{\mathbf{\alpha}}} = \dot{\lambda}_{vp} \ \left( \mathbf{\tilde{N}} -
     \frac{\gamma}{C} \ \beta \right ) \\
-             \dot{D} = \dot{\lambda}_{d}\\
-             \mathbf{\tilde{N}} = \frac{3}{2} \frac{\mathbf{\tau}}{J_2(\mathbf{\tau})}\\
-             \mathbf{\tau}= \frac{\mathbf{\sigma}^{\rm D}}{1-D} - \mathbf{\beta^{\rm D}}\\
+             \dot{D} = \dot{\lambda}_{d} \f]
+             with \f$ \mathbf{\tilde{N}} = \frac{3}{2} \frac{\mathbf{\tau}}{J_2(\mathbf{\tau})}\f$
+    and \f$ \mathbf{\tau}= \frac{\mathbf{\sigma}^{\rm D}}{1-D} - \mathbf{\beta^{\rm D}} \f$
     */
     void update_internal_variables(double const time_step_size) override;
 
@@ -38,22 +40,24 @@ public:
 
 protected:
     /**
-     * Performs the radial return algorithm with nonlinear kinematic hardening.
-     * This provides the plastic increment required for updating the internal variables
-      * This is done by solving the following system of equations
+     * Performs the radial return algorithm with nonlinear kinematic hardening and damage.
+     * This provides the plastic increment required for updating the plastic strain. cauchy_stress,
+     back_stress, damage_var, kin_hard, energy_var and C_algorithmic, are updated within this
+     routine.
+      * This is done by solving the following nonlinear system of equations:
       * \f[ \dot{\lambda}_{vp} - k_{vp} \langle f_{vp} \rangle^{n_{vp}} = 0 \\
      * \dot{\lambda}_{d} - k_{d} \langle f_{d} \rangle^{n_{d}} = 0 \\
      * \mathbf{\sigma}-(1-D)\ \mathbb{C}\ {\mathbf{\varepsilon}}^{e}_{tr} + \mathbb{C}\
-     \Delta{\lambda}_{vp} \ \mathbb{\tilde{N}} = \mathbf{0} \\
-     * \mathbf{\beta}-C \ \alpha_n - C * \Delta {\lambda}_{vp} \ \left(
-     \mathbb{\tilde{N}}-\frac{\gamma}{C} \ \beta \right)  = \mathbf{0} \\
+     \Delta{\lambda}_{vp} \ \mathbf{\tilde{N}} = \mathbf{0} \\
+     * \mathbf{\beta}-C \ \alpha_n - C \ \Delta {\lambda}_{vp} \ \left(
+     \mathbf{\tilde{N}}-\frac{\gamma}{C} \ \beta \right)  = \mathbf{0} \\
      * D-D_n-\Delta \ {\lambda}_{d} = 0 \\
      * Y - \frac{1}{2} \left( {\mathbf{\varepsilon}}^{e}_{tr} - \Delta {\lambda}_{vp} \
-     \mathbb{\tilde{N}}\ \frac{1}{1-D} \right ) : \mathbb{C} : \left(
+     \mathbf{\tilde{N}}\ \frac{1}{1-D} \right ) : \mathbb{C} : \left(
      {\mathbf{\varepsilon}}^{e}_{tr}
      - \Delta
-     {\lambda}_{vp} \ \mathbb{\tilde{N}} \ \frac{1}{1-D} \right ) = 0 \f]
-      * Note that all the n+1 indices are ommited for the sake of simplicity
+     {\lambda}_{vp} \ \mathbf{\tilde{N}} \ \frac{1}{1-D} \right ) = 0 \f]
+      * Note that all the n+1 indices are ommited for the sake of simplicity.
       */
     [[nodiscard]] double perform_radial_return(Matrix3& cauchy_stress,
                                                Matrix3& back_stress,
