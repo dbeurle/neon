7fa6cfb9719a4d4bde677aa6b52817e7650e9beb
Darcy Beurle <darcy@neonfe.io>
2017-12-16 19:21:44 +0100

    Added type aliases for lower case consistent with std convention

--- src/numeric/DenseMatrix.hpp
+++ src/numeric/DenseMatrix.hpp
@@ -18,7 +18,6 @@ using Matrix2 = Eigen::Matrix<double, 2, 2>;
 using Matrix3 = Eigen::Matrix<double, 3, 3>;
 using Matrix6 = Eigen::Matrix<double, 6, 6>;
 using Matrix9 = Eigen::Matrix<double, 9, 9>;
-
 /** Compile time fixed rows for nodal coordinates in two dimensions */
 using Matrix2x = Eigen::Matrix<double, 2, Eigen::Dynamic>;
 /** Compile time fixed rows for nodal coordinates in three dimensions */
@@ -28,4 +27,22 @@ using Vector = Eigen::Matrix<double, Eigen::Dynamic, 1>;
 using Vector2 = Eigen::Vector2d;
 using Vector3 = Eigen::Vector3d;
 using Vector6 = Eigen::Matrix<double, 6, 1>;
+
+using matrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+using rowmatrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+
+using matrix2 = Eigen::Matrix<double, 2, 2>;
+using matrix3 = Eigen::Matrix<double, 3, 3>;
+using matrix6 = Eigen::Matrix<double, 6, 6>;
+using matrix9 = Eigen::Matrix<double, 9, 9>;
+
+/** Compile time fixed rows for nodal coordinates in two dimensions */
+using matrix2x = Eigen::Matrix<double, 2, Eigen::Dynamic>;
+/** Compile time fixed rows for nodal coordinates in three dimensions */
+using matrix3x = Eigen::Matrix<double, 3, Eigen::Dynamic>;
+
+using vector = Eigen::Matrix<double, Eigen::Dynamic, 1>;
+using vector2 = Eigen::Vector2d;
+using vector3 = Eigen::Vector3d;
+using vector6 = Eigen::Matrix<double, 6, 1>;
 }
--- src/numeric/IndexTypes.hpp
+++ src/numeric/IndexTypes.hpp
@@ -10,4 +10,7 @@ using int32 = std::int32_t;
 using int64 = std::int64_t;
 
 using List = std::vector<int32>;
+
+using local_indices = std::vector<std::int32_t>;
+using nonlocal_indices = std::vector<std::int64_t>;
 }
