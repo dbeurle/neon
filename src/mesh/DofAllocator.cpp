6e49f45dd9c746b2ca43a690cdd10d7ad2ca3335
Darcy Beurle <darcy@neonfe.io>
2017-12-18 09:04:17 +0100

    Performance increase for dof allocation

--- src/mesh/DofAllocator.cpp
+++ src/mesh/DofAllocator.cpp
@@ -5,8 +5,41 @@
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/transform.hpp>
 
+#include <algorithm>
+
 namespace neon
 {
+local_indices allocate_element_dofs(int32 const nodal_dofs, local_indices const& element_nodes)
+{
+    local_indices element_dof_list;
+
+    element_dof_list.reserve(nodal_dofs * element_nodes.size());
+
+    for (auto const& node : element_nodes)
+    {
+        for (auto dof_offset = 0; dof_offset < nodal_dofs; ++dof_offset)
+        {
+            element_dof_list.emplace_back(node * nodal_dofs + dof_offset);
+        }
+    }
+    return element_dof_list;
+}
+
+std::vector<local_indices> allocate_dof_list(int32 const nodal_dofs,
+                                             std::vector<local_indices> const& element_node_list)
+{
+    std::vector<local_indices> element_dof_list(element_node_list.size());
+
+    std::transform(std::begin(element_node_list),
+                   std::end(element_node_list),
+                   std::begin(element_dof_list),
+                   [&](auto const& element_nodes) {
+                       return allocate_element_dofs(nodal_dofs, element_nodes);
+                   });
+
+    return element_dof_list;
+}
+
 std::vector<local_indices> filter_dof_list(int32 const nodal_dofs,
                                            int32 const dof_offset,
                                            std::vector<local_indices> const& nodal_connectivity)
