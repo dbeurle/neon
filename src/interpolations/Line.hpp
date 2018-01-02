fd48a44cb432b49637f15b932cea089384c69188
Darcy Beurle <darcy@neonfe.io>
2018-01-02 16:20:33 +0100

    Fixed coordinates courtesy of Ali Hussein.

--- src/interpolations/Line.hpp
+++ src/interpolations/Line.hpp
@@ -20,8 +20,8 @@ protected:
     /**
      * Initialize the shape functions to the following polynomials
      * \f{align*}{
-     * N_1(r, s, t) &= \frac{1}{2}\left(1 - \xi \right) \\
-     * N_2(r, s, t) &= \frac{1}{2}\left(1 + \xi \right)
+     * N_1(\xi) &= \frac{1}{2}\left(1 - \xi \right) \\
+     * N_2(\xi) &= \frac{1}{2}\left(1 + \xi \right)
      * \f}
      */
     void precompute_shape_functions();
@@ -41,9 +41,9 @@ protected:
     /**
      * Initialize the shape functions to the following polynomials
      * \f{align*}{
-     * N_1(r, s, t) &= \frac{1}{2} \xi \left(\xi - 1 \right) \\
-     * N_2(r, s, t) &= 1 - \xi^2 \\
-     * N_3(r, s, t) &= \frac{1}{2} \xi \left(\xi + 1 \right)
+     * N_1(\xi) &= \frac{1}{2} \xi \left(\xi - 1 \right) \\
+     * N_2(\xi) &= 1 - \xi^2 \\
+     * N_3(\xi) &= \frac{1}{2} \xi \left(\xi + 1 \right)
      * \f}
      */
     void precompute_shape_functions();
