0be81c7b70b0497cb17ce8b7c6df1d9d31609de4
Darcy Beurle <darcy@neonfe.io>
2018-02-14 10:31:48 +0100

    Something like this for the short term traits system? Issue #45.

--- src/traits/mechanics.hpp
+++ src/traits/mechanics.hpp
@@ -3,25 +3,22 @@
 
 #include <cstdint>
 
-namespace neon::mechanics
+namespace neon::mechanical
 {
-enum class type : uint8_t { plane_stress, plane_strain, axisymmetric, solid, plate, shell };
+enum class type : uint8_t { plane_stress, plane_strain, axisymmetric, solid, shell, beam };
 
-template <neon::mechanics::type T, bool is_symmetric_>
+template <type T, bool is_symmetric_>
 struct traits;
 
 template <>
 struct traits<type::plane_strain, bool is_symmetric_ = true>
 {
-    static auto constexpr value = type::plane_strain;
+    static auto constexpr value{type::plane_strain};
+    static auto constexpr size{2};
+    static auto constexpr is_symmetric{is_symmetric_};
 
-    static auto constexpr size = 2;
-
-    static auto constexpr is_symmetric = is_symmetric_;
-
-    using constitutive_type = constitutive_model<value>;
-
-    using mesh_type = fem_mesh<value>;
+    /** Plane stress mechanics has two unknowns (u_x, u_y) */
+    static std::array<std::int32_t, 3> constexpr dofs{0, 1};
 
     using rank_two_tensor = matrix2;
     using rank_four_tensor = matrix3;
@@ -31,14 +28,11 @@ template <>
 struct traits<type::plane_stress, bool is_symmetric_ = true>
 {
     static auto constexpr value = type::plane_stress;
-
-    static auto constexpr size = 2;
-
+    static auto constexpr size{2};
     static auto constexpr is_symmetric = is_symmetric_;
 
-    using constitutive_type = constitutive_model<value>;
-
-    using mesh_type = fem_mesh<value>;
+    /** Plane stress mechanics has two unknowns (u_x, u_y) */
+    static std::array<std::int32_t, 3> constexpr dofs{0, 1};
 
     using rank_two_tensor = matrix2;
     using rank_four_tensor = matrix3;
@@ -47,20 +41,16 @@ struct traits<type::plane_stress, bool is_symmetric_ = true>
 template <>
 struct traits<type::solid, bool is_symmetric_ = true>
 {
-    static auto constexpr value = type::solid;
+    static auto constexpr value{type::solid};
+    static auto constexpr size{3};
+    static auto constexpr is_symmetric{is_symmetric_};
 
-    /** Solid continuum mechanics has three dimension (x, y, z) */
-    static auto constexpr size = 3;
+    /** Solid mechanics has three unknowns (u_x, u_y, u_z) */
+    static std::array<std::int32_t, 3> constexpr dofs{0, 1, 2};
 
     /** matrix type for the deformation and stress vector */
     using rank_two_tensor = matrix3;
-
     /** matrix type for a fourth order tensor */
-    using rank_four_tensor = std::conditional<is_symmetric_, matrix6, matrix9>;
-
-    using internal_variable_type = internal_variable<rank_two_tensor, rank_four_tensor>;
-    using constitutive_type = constitutive_model<internal_variable_type>;
-
-    using mesh_type = fem_mesh<value>;
+    using rank_four_tensor = std::conditional<is_symmetric, matrix6, matrix9>::type;
 };
 }
