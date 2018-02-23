96d153cc42462465301aef4c4b4e3d25a4ed86ef
Darcy Beurle <darcy@neonfe.io>
2018-02-23 14:39:23 +0100

    Boundary condition view updates

--- src/mesh/mechanical/solid/boundary/pressure.hpp
+++ src/mesh/mechanical/solid/boundary/pressure.hpp
@@ -20,7 +20,7 @@ class pressure : public traction
 public:
     using traction::traction;
 
-    std::tuple<local_indices const&, vector> external_force(int const element,
-                                                            double const load_factor) const override;
+    std::pair<index_view, vector> external_force(std::int64_t const element,
+                                                 double const load_factor) const override;
 };
 }
