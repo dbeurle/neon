e4b416e61cf43ee261cfe15a9227e33f47c6cc3a
Darcy Beurle <darcy@neonfe.io>
2018-03-03 18:38:12 +0100

    Added routines for the standard and general eigenvalues

--- src/solver/eigen/eigen_solver.cpp
+++ src/solver/eigen/eigen_solver.cpp
@@ -12,18 +12,35 @@ eigen_solver::eigen_solver(std::int64_t const eigenvalues_to_extract)
 {
 }
 
-std::pair<vector, matrix> eigen_solver::solve(sparse_matrix const& A, sparse_matrix const& B)
+std::pair<vector, matrix> eigen_solver::solve(sparse_matrix const& A)
 {
-    // using solver_type = Eigen::SimplicialLU<Eigen::SparseMatrix<sparse_matrix::Scalar>>;
-    // using solver_type = Eigen::SparseLU<sparse_matrix, Eigen::AMDOrdering<std::int32_t>>;
-
     Eigen::SparseMatrix<double> A_col = A;
 
-    std::cout << "Different ordering of A\n" << A_col << "\n";
+    Eigen::ArpackGeneralizedSelfAdjointEigenSolver<decltype(A_col)> arpack;
 
-    Eigen::ArpackGeneralizedSelfAdjointEigenSolver<Eigen::SparseMatrix<double>> arpack;
+    arpack.compute(A_col, eigenvalues_to_extract, "LM");
 
-    arpack.compute(A_col, eigenvalues_to_extract, "SM");
+    if (arpack.getNbrConvergedEigenValues() < eigenvalues_to_extract)
+    {
+        throw computational_error("Eigenvalues did not converge");
+    }
+
+    if (arpack.info() != Eigen::Success)
+    {
+        throw computational_error("Numerical issued occurred");
+    }
+
+    return {arpack.eigenvectors(), arpack.eigenvalues()};
+}
+
+std::pair<vector, matrix> eigen_solver::solve(sparse_matrix const& A, sparse_matrix const& B)
+{
+    Eigen::SparseMatrix<double> A_col = A;
+    Eigen::SparseMatrix<double> B_col = B;
+
+    Eigen::ArpackGeneralizedSelfAdjointEigenSolver<decltype(A_col)> arpack;
+
+    arpack.compute(A_col, B_col, eigenvalues_to_extract, "LM");
 
     if (arpack.getNbrConvergedEigenValues() < eigenvalues_to_extract)
     {
--- src/solver/eigen/eigen_solver.hpp
+++ src/solver/eigen/eigen_solver.hpp
@@ -14,10 +14,13 @@ class eigen_solver
 public:
     eigen_solver(std::int64_t const eigenvalues_to_extract);
 
-    /** Solve the generalised eigenvalue problem $\f (A - \lambda B) I = 0 $\f */
+    /** Solve the standard eigenvalue problem $\f (A - \lambda I) x = 0 $\f */
+    std::pair<vector, matrix> solve(sparse_matrix const& A);
+
+    /** Solve the generalised eigenvalue problem $\f (A - \lambda B) x = 0 $\f */
     std::pair<vector, matrix> solve(sparse_matrix const& A, sparse_matrix const& B);
 
 protected:
-    std::int64_t eigenvalues_to_extract{1};
+    std::int64_t eigenvalues_to_extract{};
 };
 }
