433c9e629e0ce0580855a5c5a8bcef502685b99f
Darcy Beurle <dbeurle@users.noreply.github.com>
2018-03-04 16:46:20 +0100

    Maintenance (#57)
    * Name change for linear solvers
    
    * Introduce arpack eigenvalue solver
    
    * WIP for arpack-ng and the eigen wrapper
    
    * Use the eigenvalue solver
    
    * Remove nvidia helper functions
    
    * Update cmake build scripts
    
    * Integer comparison fixes
    
    * Added compiler check to avoid unknown pragma warnings
    
    * Eigenvalue solver updates
    
    * Added routines for the standard and general eigenvalues
    
    * Use a larger set
    
    * Refinements to the CMake script
    
    * Variable name update.  Changed some storage to matrices

---
+++ src/solver/eigen/eigen_solver.cpp
@@ -0,0 +1,57 @@
+
+#include "eigen_solver.hpp"
+#include "Exceptions.hpp"
+
+#include <iostream>
+#include "arpack_ng/ArpackSelfAdjointEigenSolver.h"
+
+namespace neon
+{
+eigen_solver::eigen_solver(std::int64_t const eigenvalues_to_extract)
+    : eigenvalues_to_extract{eigenvalues_to_extract}
+{
+}
+
+std::pair<vector, matrix> eigen_solver::solve(sparse_matrix const& A)
+{
+    Eigen::SparseMatrix<double> A_col = A;
+
+    Eigen::ArpackGeneralizedSelfAdjointEigenSolver<decltype(A_col)> arpack;
+
+    arpack.compute(A_col, eigenvalues_to_extract, "LM");
+
+    if (arpack.getNbrConvergedEigenValues() < eigenvalues_to_extract)
+    {
+        throw computational_error("Eigenvalues did not converge");
+    }
+
+    if (arpack.info() != Eigen::Success)
+    {
+        throw computational_error("Numerical issued occurred");
+    }
+
+    return {arpack.eigenvectors(), arpack.eigenvalues()};
+}
+
+std::pair<vector, matrix> eigen_solver::solve(sparse_matrix const& A, sparse_matrix const& B)
+{
+    Eigen::SparseMatrix<double> A_col = A;
+    Eigen::SparseMatrix<double> B_col = B;
+
+    Eigen::ArpackGeneralizedSelfAdjointEigenSolver<decltype(A_col)> arpack;
+
+    arpack.compute(A_col, B_col, eigenvalues_to_extract, "LM");
+
+    if (arpack.getNbrConvergedEigenValues() < eigenvalues_to_extract)
+    {
+        throw computational_error("Eigenvalues did not converge");
+    }
+
+    if (arpack.info() != Eigen::Success)
+    {
+        throw computational_error("Numerical issued occurred");
+    }
+
+    return {arpack.eigenvalues(), arpack.eigenvectors()};
+}
+}
---
+++ src/solver/eigen/eigen_solver.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#include "numeric/dense_matrix.hpp"
+#include "numeric/sparse_matrix.hpp"
+
+#include <cstdint>
+#include <utility>
+
+namespace neon
+{
+class eigen_solver
+{
+public:
+    eigen_solver(std::int64_t const eigenvalues_to_extract);
+
+    /** Solve the standard eigenvalue problem $\f (A - \lambda I) x = 0 $\f */
+    std::pair<vector, matrix> solve(sparse_matrix const& A);
+
+    /** Solve the generalised eigenvalue problem $\f (A - \lambda B) x = 0 $\f */
+    std::pair<vector, matrix> solve(sparse_matrix const& A, sparse_matrix const& B);
+
+protected:
+    std::int64_t eigenvalues_to_extract{};
+};
+}
