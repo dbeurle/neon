f14132d01d502a41cf5ac90ec4df5ed737e8e2ce
Darcy Beurle <dbeurle@users.noreply.github.com>
2018-03-06 17:47:32 +0100

    Add missing delta t

--- src/solver/time/runge_kutta_integration.hpp
+++ src/solver/time/runge_kutta_integration.hpp
@@ -16,9 +16,9 @@ auto runge_kutta_second_order(functor&& f)
         static_assert(std::is_floating_point<decltype(t)>::value);
         static_assert(std::is_floating_point<decltype(dt)>::value);
 
-        auto const dy1 = f(t, y);
-        auto const dy2 = f(t + dt, y + dy1);
-        return dt * (dy1 + dy2) / 2.0;
+        auto const dy1 = dt * f(t, y);
+        auto const dy2 = dt * f(t + dt, y + dy1);
+        return (dy1 + dy2) / 2.0;
     };
 }
 
@@ -33,11 +33,11 @@ auto runge_kutta_fourth_order(functor&& f)
         static_assert(std::is_floating_point<decltype(t)>::value);
         static_assert(std::is_floating_point<decltype(dt)>::value);
 
-        auto const dy1 = f(t, y);
-        auto const dy2 = f(t + dt / 2.0, y + dy1 / 2.0);
-        auto const dy3 = f(t + dt / 2.0, y + dy2 / 2.0);
-        auto const dy4 = f(t + dt, y + dy3);
-        return dt * (dy1 + 2.0 * dy2 + 2.0 * dy3 + dy4) / 6.0;
+        auto const dy1 = dt * f(t, y);
+        auto const dy2 = dt * f(t + dt / 2.0, y + dy1 / 2.0);
+        auto const dy3 = dt * f(t + dt / 2.0, y + dy2 / 2.0);
+        auto const dy4 = dt * f(t + dt, y + dy3);
+        return (dy1 + 2.0 * dy2 + 2.0 * dy3 + dy4) / 6.0;
     };
 }
 }
