ea55a6b809e0ceb4c924b5cbde11d716c5e8bbb3
Darcy Beurle <darcy@neonfe.io>
2018-03-30 10:10:28 +0200

    Remove unneeded methods

--- src/material/micromechanical_elastomer.cpp
+++ src/material/micromechanical_elastomer.cpp
@@ -45,29 +45,3 @@ ageing_micromechanical_elastomer::ageing_micromechanical_elastomer(json const& m
         throw std::domain_error("Material properties (probabilities) must be positive");
     }
 }
-
-double ageing_micromechanical_elastomer::compute_new_segment(double const current_segment,
-                                                             double const time_step_size) const
-{
-    return current_segment / (1.0 + segment_decay_rate * time_step_size);
-}
-
-double ageing_micromechanical_elastomer::compute_new_shear_modulus(double const time_step_size) const
-{
-    return crosslink_growth_rate * time_step_size;
-}
-
-std::vector<double> ageing_micromechanical_elastomer::scission(std::vector<double> shear_moduli,
-                                                               std::vector<double> const& segments,
-                                                               double const time_step_size) const
-{
-    std::transform(std::begin(shear_moduli),
-                   std::end(shear_moduli),
-                   std::begin(segments),
-                   std::begin(shear_moduli),
-                   [this, time_step_size](auto const G, auto const N) {
-                       return G / (1.0 + time_step_size * (1.0 - std::pow(1.0 - pr_scission, N)));
-                   });
-
-    return shear_moduli;
-}
--- src/material/micromechanical_elastomer.hpp
+++ src/material/micromechanical_elastomer.hpp
@@ -39,44 +39,8 @@ public:
     /// \return The probability per unit time of chains recombining
     [[nodiscard]] double recombination_probability() const noexcept { return pr_recombination; }
 
-    /// \return The constant rate of active segment decay
-    [[nodiscard]] double active_segment_decay_rate() const noexcept
-    {
-        return crosslink_growth_rate;
-    }
-
-    /// Update the next number of segments in the history based on the current
-    /// number of segments.  This rate is controlled by \p SegmentDecayRate in
-    /// the material properties section of the input file.
-    /// \param current_segment  Last average number of segments per chain
-    /// \param time_step_size Time step size for time discretisation
-    /// \return A new segments which is less than current_segment
-    [[nodiscard]] double compute_new_segment(double const current_segment,
-                                             double const time_step_size) const;
-
-    /// Update the shear modulus based on the initial shear modulus
-    /// so the newly created secondary networks occur uniformly
-    /// \param time_step_size Time step size for time discretisation
-    /// \return A new partial shear modulus
-    [[nodiscard]] double compute_new_shear_modulus(double const time_step_size) const;
-
-    /// Compute the updated shear modulus based on the creation of cross-links
-    /// causing additional chains with fewer average segments per chain
-    /// and the removal of active chains based on the average segments per group
-    /// and the rate at which the removal occurs.
-    /// \param current_shear_moduli
-    /// \param current_segments
-    /// \param time_step_size Time step size for time discretisation
-    /// \return The new shear modulus based on the average segments per chain
-    [[nodiscard]] std::vector<double> scission(std::vector<double> shear_moduli,
-                                               std::vector<double> const& segments,
-                                               double const time_step_size) const;
-
 protected:
     double pr_scission{0.0};      /// Scission probability
     double pr_recombination{0.0}; /// Recombination probability
-
-    double crosslink_growth_rate{0.0}; /// Rate of new crosslinks
-    double segment_decay_rate{0.0};    /// Rate of average segments per chain decay
 };
 }
