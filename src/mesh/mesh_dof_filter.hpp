4ca59d2bd673547b193d234e1b9837350987a2e0
Darcy Beurle <darcy@neonfe.io>
2018-04-23 23:27:35 +0200

    Remove unneeded inline specifier

--- src/mesh/mesh_dof_filter.hpp
+++ src/mesh/mesh_dof_filter.hpp
@@ -10,8 +10,7 @@
 namespace neon
 {
 template <std::int8_t dof_size, typename boundary_mesh_type>
-[[nodiscard]] inline std::vector<std::int32_t> mesh_dof_filter(boundary_mesh_type const& boundary_meshes)
-{
+[[nodiscard]] std::vector<std::int32_t> mesh_dof_filter(boundary_mesh_type const& boundary_meshes) {
     std::set<std::int32_t> dof_set;
 
     for (auto const& boundary_mesh : boundary_meshes)
