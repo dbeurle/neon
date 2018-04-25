d74bb87d7d8ccec0511e55bfcf661cec0438af31
Darcy Beurle <darcy@neonfe.io>
2018-04-25 19:19:12 +0200

    Compute jacobian determinants in free function

---
+++ src/math/jacobian_determinant.hpp
@@ -0,0 +1,49 @@
+
+#pragma once
+
+#include "numeric/dense_matrix.hpp"
+#include <Eigen/Geometry>
+
+/// \file jacobian_determinant.hpp
+
+namespace neon
+{
+/**
+ * Compute the Jacobian determinant for a volume or surface mapping.  In three
+ * dimensions it performs the following operation:
+ * \f{align*}{
+ *     j &= \det \begin{bmatrix}
+ *             \frac{\partial x}{\partial \xi} & \frac{\partial x}{\partial \eta} & \frac{\partial x}{\partial \zeta} \\
+ *             \frac{\partial y}{\partial \xi} & \frac{\partial y}{\partial \eta} & \frac{\partial y}{\partial \zeta} \\
+ *             \frac{\partial z}{\partial \xi} & \frac{\partial z}{\partial \eta} & \frac{\partial z}{\partial \zeta}
+ *          \end{bmatrix}
+ * \f}
+ */
+template <typename matrix_type>
+[[nodiscard]] auto jacobian_determinant(matrix_type const& jacobian)
+{
+    return jacobian.determinant();
+}
+
+/**
+ * Compute the Jacobian determinant for non-square \f$ \mathbb{R}^3 \f$ to \f$ \mathbb{R}^2 \f$ Jacobian
+ * using the result from calculus
+ * \f{align*}{
+ *     j &= || \mathbf{x}_{,\mathbf{\xi}} \times \mathbf{x}_{,\mathbf{\eta}} ||
+ * \f}
+ * where the Jacobian is given by the non-square matrix
+ * \f{align*}{
+ *    & \begin{bmatrix}
+ *          \frac{\partial x}{\partial \xi} & \frac{\partial x}{\partial \eta} \\
+ *          \frac{\partial y}{\partial \xi} & \frac{\partial y}{\partial \eta} \\
+ *          \frac{\partial z}{\partial \xi} & \frac{\partial z}{\partial \eta}
+ *      \end{bmatrix}
+ * \f}
+ * This routine is useful when the surface is described by a two dimensional
+ * element but there are only three dimensional coordinates \cite Anton1998.
+ */
+[[nodiscard]] inline auto jacobian_determinant(matrix32 const& jacobian)
+{
+    return jacobian.col(0).cross(jacobian.col(1)).norm();
+}
+}
