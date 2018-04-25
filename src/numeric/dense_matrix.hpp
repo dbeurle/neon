b5cef510984b40a773a85fc73886ab61f315192a
Darcy Beurle <darcy@neonfe.io>
2018-04-25 19:21:09 +0200

    Add documentation

--- src/numeric/dense_matrix.hpp
+++ src/numeric/dense_matrix.hpp
@@ -9,17 +9,26 @@
 #include <Eigen/Core>
 #include <Eigen/LU>
 
+/// \file dense_matrix.hpp
+
 namespace neon
 {
 using matrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 using row_matrix = matrix;
 using col_matrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor>;
 
+/// 2x2 fixed size matrix
 using matrix2 = Eigen::Matrix<double, 2, 2>;
+/// 3x3 fixed size matrix
 using matrix3 = Eigen::Matrix<double, 3, 3>;
+/// 6x6 fixed size matrix
 using matrix6 = Eigen::Matrix<double, 6, 6>;
+/// 9x9 fixed size matrix
 using matrix9 = Eigen::Matrix<double, 9, 9>;
+/// 16x16 fixed size matrix
 using matrix16 = Eigen::Matrix<double, 16, 16>;
+/// 3x2 fixed size matrix for non-square Jacobians
+using matrix32 = Eigen::Matrix<double, 3, 2>;
 
 template <int geometric_dimension>
 using matrixxd = Eigen::Matrix<double, Eigen::Dynamic, geometric_dimension>;
