892de0c53870728fffd3b17c67fbbe2276bfc4b1
Darcy Beurle <darcy@neonfe.io>
2018-05-03 19:44:28 +0200

    WIP profile factory

--- src/geometry/profile.hpp
+++ src/geometry/profile.hpp
@@ -11,12 +11,20 @@ namespace neon::geometry
 class profile
 {
 public:
-    explicit profile(double const I_1, double const I_2) : I_1{I_1}, I_2{I_2} {}
+    explicit profile(
+        double const I_1, double const I_2, double const A, double const A_1, double const A_2)
+        : I_1{I_1}, I_2{I_2}, m_area{A}, A_1{A_1}, A_2{A_2}
+    {
+    }
 
-    /// \return pair of first and second moment of inertia
+    /// \return First and second moment of inertia
     auto second_moment_area() const noexcept { return std::pair{I_1, I_2}; }
 
+    /// \return First and second shear area
     auto shear_area() const noexcept { return std::pair{A_1, A_2}; }
+
+    /// \return Cross-section area
+    auto area() const noexcept { return m_area; }
 
 protected:
     /// Area moment of inertia (first coordinate)
@@ -24,6 +32,8 @@ protected:
     /// Area moment of inertia (second coordinate)
     double I_2{0.0};
 
+    /// Section area
+    double m_area;
     /// Shear area (first coordinate)
     double A_1{0.0};
     /// Shear area (first coordinate)
@@ -36,7 +46,11 @@ class rectangular_bar : public profile
 {
 public:
     explicit rectangular_bar(double const width, double const height)
-        : profile(width * std::pow(height, 3) / 12.0, std::pow(width, 3) * height / 12.0)
+        : profile(width * std::pow(height, 3) / 12.0,
+                  std::pow(width, 3) * height / 12.0,
+                  width * height,
+                  width * height,
+                  width * height)
     {
     }
 };
@@ -47,7 +61,11 @@ class hollow_rectangular_bar : public profile
 {
 public:
     hollow_rectangular_bar(double const width, double const height)
-        : profile(width * std::pow(height, 3) / 12.0, std::pow(width, 3) * height / 12.0)
+        : profile(width * std::pow(height, 3) / 12.0,
+                  std::pow(width, 3) * height / 12.0,
+                  width * height,
+                  width * height,
+                  width * height)
     {
     }
 };
