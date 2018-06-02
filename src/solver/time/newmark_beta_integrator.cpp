cc8dee8e0ee0725f8f2444be3326009e67aff2a2
Darcy Beurle <darcy@neonfe.io>
2018-06-02 10:03:37 +0200

    Move header declarations to cpp file

--- src/solver/time/newmark_beta_integrator.cpp
+++ src/solver/time/newmark_beta_integrator.cpp
@@ -56,4 +56,45 @@ bool newmark_beta_integrator::are_parameters_unstable() const
     // Unconditional stability condition
     return beta_parameter < artifical_viscosity / 2.0 || artifical_viscosity / 2.0 < 0.25;
 }
+
+double newmark_beta_integrator::mass_scaling_factor() const
+{
+    auto const time_step_size = time_control.current_time_step_size();
+    return 1.0 / (beta_parameter * std::pow(time_step_size, 2));
+}
+
+double newmark_beta_integrator::time_step_size() const
+{
+    return time_control.current_time_step_size();
+}
+
+vector newmark_beta_integrator::approximate_displacements(vector const& a,
+                                                          vector const& v,
+                                                          vector const& d) const
+{
+    auto const time_step_size = time_control.current_time_step_size();
+
+    return d + time_step_size * v
+           + std::pow(time_step_size, 2) / 2.0 * (1.0 - 2.0 * beta_parameter) * a;
+}
+
+vector newmark_beta_integrator::approximate_velocities(vector const& a, vector const& v) const
+{
+    auto const time_step_size = time_control.current_time_step_size();
+    return v + (1.0 - artifical_viscosity) * time_step_size * a;
+}
+
+vector newmark_beta_integrator::accelerations(vector const& a, vector const& v, vector const& d)
+{
+    auto const time_step_size = time_control.current_time_step_size();
+    a_old = a;
+
+    return 1.0 / (beta_parameter * std::pow(time_step_size, 2))
+           * (d - approximate_displacements(a, v, d));
+}
+vector newmark_beta_integrator::velocities(vector const& a, vector const& v) const
+{
+    auto const time_step_size = time_control.current_time_step_size();
+    return approximate_velocities(a_old, v) + artifical_viscosity * time_step_size * a;
+}
 }
