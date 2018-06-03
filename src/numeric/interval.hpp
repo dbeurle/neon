447eb34dee5b50d4377239c179d708372468b612
Darcy Beurle <darcy@neonfe.io>
2018-06-03 17:15:53 +0200

    Add an initial pass at interval for time integrations for addition later

---
+++ src/numeric/interval.hpp
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include <cstdint>
+
+/// \file interval.hpp
+
+namespace neon
+{
+/// \return Number of intervals between \p start and \p end with a \p step_size as a 64 bit integer
+[[nodiscard]] inline std::int64_t interval(double const start, double const end, double const step_size)
+{
+    if (end <= start)
+    {
+        throw std::domain_error("The end time must be greater than the start time");
+    }
+    return (end - start) / step_size;
+}
+}
