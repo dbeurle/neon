d50fe6f5d9a25c2be6c7e774af9098c5d7c84d39
Darcy Beurle <darcy@beurle.id.au>
2018-06-26 14:32:51 +0200

    Test out coverage analysis

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -3,10 +3,7 @@ if(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
     message(FATAL_ERROR "Coverage only available when compiling with GCC.")
 endif()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
-
 add_custom_target(coverage
-                  COMMAND ctest
                   COMMAND lcov --capture --directory ${CMAKE_BINARY_DIR} --output-file coverage.info
                   # Remove the external libraries to get coverage for neon source only
                   COMMAND lcov --remove coverage.info '/usr/*'
@@ -15,6 +12,6 @@ add_custom_target(coverage
                                                       '${CMAKE_BINARY_DIR}/json*'
                                                       '${CMAKE_BINARY_DIR}/range-v3*'
                                                       '${CMAKE_BINARY_DIR}/termcolor*'
-                                                      -o coverage.info)
+                                                      '${CMAKE_SOURCE_DIR}/tests*' -o coverage.info)
                   # COMMAND genhtml coverage.info --output-directory coverage_output
                   # COMMAND firefox coverage_output/index.html)
