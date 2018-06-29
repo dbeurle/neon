0bae6f4a789b4639f75c6e8356a4ba10a64bbba3
Darcy Beurle <darcy@beurle.id.au>
2018-06-29 08:14:57 +0200

    Catch by polymorphic reference instead

--- src/neon.cpp
+++ src/neon.cpp
@@ -12,21 +12,13 @@ int main(int argc, char* argv[])
         std::cerr << "No input file was provided.  Use <filename>.json\n";
         return 1;
     }
-
     try
     {
         neon::simulation_parser simulation(argv[1]);
 
         simulation.start();
     }
-    catch (std::runtime_error& error)
-    {
-        std::cout << std::endl
-                  << std::string(2, ' ') << termcolor::red << termcolor::bold << error.what()
-                  << termcolor::reset << std::flush << std::endl;
-        return 1;
-    }
-    catch (std::domain_error& error)
+    catch (std::exception& error)
     {
         std::cout << std::endl
                   << std::string(2, ' ') << termcolor::red << termcolor::bold << error.what()
