eb7a2239647f9810b90feb07e6364a4cb73fe5d1
Darcy Beurle <darcy@beurle.id.au>
2018-07-10 22:50:19 +0200

    Use comparison operator

--- tests/sequence.cpp
+++ tests/sequence.cpp
@@ -9,12 +9,12 @@ TEST_CASE("one sequence")
 
     REQUIRE(sequence.size() == 10);
 
-    REQUIRE(sequence[0] = 0);
-    REQUIRE(sequence[1] = 1);
-    REQUIRE(sequence[2] = 2);
-    REQUIRE(sequence[10] = 10);
-    REQUIRE(sequence[20] = 20);
-    REQUIRE(sequence[30] = 30);
-    REQUIRE(sequence[40] = 40);
-    REQUIRE(sequence[50] = 50);
+    REQUIRE(sequence[0] == 0);
+    REQUIRE(sequence[1] == 1);
+    REQUIRE(sequence[2] == 2);
+    REQUIRE(sequence[10] == 10);
+    REQUIRE(sequence[20] == 20);
+    REQUIRE(sequence[30] == 30);
+    REQUIRE(sequence[40] == 40);
+    REQUIRE(sequence[50] == 50);
 }
