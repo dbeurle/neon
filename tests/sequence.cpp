d30630b38a336e6ad283c622f1b0bf1f5705075e
Darcy Beurle <darcy@beurle.id.au>
2018-07-10 21:15:15 +0200

    Additional tests for sequence

--- tests/sequence.cpp
+++ tests/sequence.cpp
@@ -6,4 +6,15 @@
 TEST_CASE("one sequence")
 {
     neon::block_sequence<> sequence(0, 10);
+
+    REQUIRE(sequence.size() == 10);
+
+    REQUIRE(sequence[0] = 0);
+    REQUIRE(sequence[1] = 1);
+    REQUIRE(sequence[2] = 2);
+    REQUIRE(sequence[10] = 10);
+    REQUIRE(sequence[20] = 20);
+    REQUIRE(sequence[30] = 30);
+    REQUIRE(sequence[40] = 40);
+    REQUIRE(sequence[50] = 50);
 }
