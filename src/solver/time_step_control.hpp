ddfc7c4d3b701b731a41d7befd1c3d1e99be7843
Darcy Beurle <darcy@beurle.id.au>
2018-07-11 07:51:03 +0200

    Add noexcept and nodiscard specifiers

--- src/solver/time_step_control.hpp
+++ src/solver/time_step_control.hpp
@@ -15,13 +15,13 @@ public:
     /** Constructor for dynamic solution */
     time_step_control(json const& time_data);
 
-    [[nodiscard]] double current_time_step_size() const { return time_step_size; }
+    [[nodiscard]] double current_time_step_size() const noexcept { return time_step_size; }
 
-    [[nodiscard]] std::int64_t number_of_time_steps() const { return time_steps; }
+    [[nodiscard]] std::int64_t number_of_time_steps() const noexcept { return time_steps; }
 
-    void increment() { ++current_time_step; }
+    void increment() noexcept { ++current_time_step; }
 
-    bool is_finished() const { return current_time_step == time_steps; }
+    [[nodiscard]] bool is_finished() const noexcept { return current_time_step == time_steps; }
 
 protected:
     std::int64_t time_steps{1}, current_time_step{0};
