cf7f7a94207681673885b77f2e73935fa763439b
Darcy Beurle <darcy@beurle.id.au>
2018-07-16 17:46:32 +0200

    Add test for doublet class

--- tests/matrix_system.cpp
+++ tests/matrix_system.cpp
@@ -5,12 +5,40 @@
 #include "mesh/material_coordinates.hpp"
 #include "mesh/mechanical/solid/fem_mesh.hpp"
 #include "assembler/mechanical/fem_static_matrix.hpp"
+#include "numeric/doublet.hpp"
 #include "io/json.hpp"
 
 #include "CubeJson.hpp"
 
 using neon::json;
 
+TEST_CASE("Doublet class")
+{
+    SECTION("Trival zero case")
+    {
+        neon::doublet row_col_val(0, 0);
+
+        REQUIRE(row_col_val.row() == 0);
+        REQUIRE(row_col_val.col() == 0);
+        REQUIRE(row_col_val.value() == Approx(0.0));
+    }
+    SECTION("Nontrivial case")
+    {
+        neon::doublet row_col_val(3, 1);
+
+        REQUIRE(row_col_val.row() == 3);
+        REQUIRE(row_col_val.col() == 1);
+        REQUIRE(row_col_val.value() == Approx(0.0));
+    }
+    SECTION("Nontrivial case reversed")
+    {
+        neon::doublet row_col_val(1, 3);
+
+        REQUIRE(row_col_val.row() == 1);
+        REQUIRE(row_col_val.col() == 3);
+        REQUIRE(row_col_val.value() == Approx(0.0));
+    }
+}
 TEST_CASE("Nonlinear system equilibrium solver test")
 {
     using fem_mesh = neon::mechanical::solid::fem_mesh;
