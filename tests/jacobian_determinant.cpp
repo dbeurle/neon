b3c8787643dcc66fe0b6c2e1f52cc65e8c9d8783
Darcy Beurle <darcy@beurle.id.au>
2018-07-16 18:18:19 +0200

    Add test for vector Jacobian

--- tests/jacobian_determinant.cpp
+++ tests/jacobian_determinant.cpp
@@ -3,22 +3,25 @@
 
 #include "math/jacobian_determinant.hpp"
 
-using neon::jacobian_determinant;
+using namespace neon;
 
 TEST_CASE("3x3")
 {
     neon::matrix3 const random_jacobian = neon::matrix3::Random();
     REQUIRE(random_jacobian.determinant() == Approx(jacobian_determinant(random_jacobian)));
 }
-
 TEST_CASE("2x2")
 {
     neon::matrix2 const random_jacobian = neon::matrix2::Random();
     REQUIRE(random_jacobian.determinant() == Approx(jacobian_determinant(random_jacobian)));
 }
-
 TEST_CASE("3x2")
 {
     neon::matrix32 const random_jacobian = neon::matrix32::Random();
     double const j = jacobian_determinant(random_jacobian);
+}
+TEST_CASE("3x1")
+{
+    neon::vector3 const random_jacobian = neon::vector3::Random();
+    REQUIRE(jacobian_determinant(random_jacobian) == Approx(random_jacobian.norm()).margin(1e-5));
 }
