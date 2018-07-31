5a250f590f18f1c88f4f39b6d7ad26c8ac25629b
Darcy Beurle <darcy@beurle.id.au>
2018-07-31 18:19:52 +0200

    Documentation change

--- src/constitutive/constitutive_model_alias.hpp
+++ src/constitutive/constitutive_model_alias.hpp
@@ -7,19 +7,19 @@ namespace mechanical
 {
 namespace solid
 {
-/** Alias for a solid mechanics type for a symmetric material model */
+/// Alias for a solid mechanics type for a symmetric material model
 using constitutive_model = neon::constitutive_model<3, 6>;
 }
 namespace plane
 {
-/** Alias for a plane strain/stress type for a symmetric material model */
+/// Alias for a plane strain/stress type for a symmetric material model
 using constitutive_model = neon::constitutive_model<2, 3>;
 }
 }
 
 namespace diffusion
 {
-/** Alias for a diffusion type for a general 3D material model */
+/// Alias for a diffusion type for a general 3D material model
 using constitutive_model = neon::constitutive_model<3, 3>;
 }
 }
