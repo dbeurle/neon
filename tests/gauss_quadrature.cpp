b124a3fb6cda97933f2b2bec0d57fbfdd28d2daa
Darcy Beurle <darcy@beurle.id.au>
2018-07-31 18:37:33 +0200

    Add test for five node pyramid

--- tests/gauss_quadrature.cpp
+++ tests/gauss_quadrature.cpp
@@ -1108,6 +1108,54 @@ TEST_CASE("Pyramid quadrature scheme test")
         REQUIRE(ranges::accumulate(p8.weights(), 0.0) == Approx(4.0 / 3.0));
         REQUIRE(ranges::accumulate(p27.weights(), 0.0) == Approx(4.0 / 3.0));
     }
+    SECTION("Five node evaluation")
+    {
+        pyramid5 element(pyramid_quadrature::point::one);
+
+        REQUIRE(element.nodes() == 5);
+        REQUIRE(element.quadrature().points() == 1);
+
+        element.quadrature().for_each([&](auto const& femval, auto const& l) {
+            auto const& [N, rhea] = femval;
+
+            REQUIRE(N.sum() == Approx(1.0));
+
+            REQUIRE(rhea.col(0).sum() == Approx(0.0).margin(ZERO_MARGIN));
+            REQUIRE(rhea.col(1).sum() == Approx(0.0).margin(ZERO_MARGIN));
+            REQUIRE(rhea.col(2).sum() == Approx(0.0).margin(ZERO_MARGIN));
+        });
+        REQUIRE(element.local_quadrature_extrapolation().rows() == 5);
+        REQUIRE(element.local_quadrature_extrapolation().cols() == 1);
+        REQUIRE(element.local_quadrature_extrapolation().allFinite());
+    }
+    SECTION("Five node volume - one point")
+    {
+        pyramid5 element(pyramid_quadrature::point::one);
+
+        auto const base = 1.0;
+        auto const height = 1.0;
+
+        matrix3x x(3, 5);
+        x << 0.0, base, base, 0.0, height / 2.0, //
+            0.0, 0.0, base, base, height / 2.0,  //
+            0.0, 0.0, 0.0, 0.0, height;
+
+        REQUIRE(element.compute_measure(x) == Approx(1.0 / 3.0));
+    }
+    SECTION("Five node volume - eight point")
+    {
+        pyramid5 element(pyramid_quadrature::point::eight);
+
+        auto const base = 1.0;
+        auto const height = 1.0;
+
+        matrix3x x(3, 5);
+        x << 0.0, base, base, 0.0, height / 2.0, //
+            0.0, 0.0, base, base, height / 2.0,  //
+            0.0, 0.0, 0.0, 0.0, height;
+
+        REQUIRE(element.compute_measure(x) == Approx(1.0 / 3.0));
+    }
 }
 TEST_CASE("Unit sphere quadrature scheme test", "[unit_sphere_quadrature]")
 {
