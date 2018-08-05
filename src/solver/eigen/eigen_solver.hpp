d96ae904931e897f71f61c9a38dda35af1d75349
Darcy Beurle <darcy@beurle.id.au>
2018-08-05 21:11:08 +0200

    Documentation

--- src/solver/eigen/eigen_solver.hpp
+++ src/solver/eigen/eigen_solver.hpp
@@ -15,11 +15,11 @@ public:
     eigen_solver(std::int64_t const eigenvalues_to_extract);
 
     /// Solve the standard eigenvalue problem $\f (A - \lambda I) x = 0 $\f
-    /// \return Eigenvalues and vectors
+    /// \return eigenvalues and vectors
     std::pair<vector, matrix> solve(sparse_matrix const& A);
 
     /// Solve the generalised eigenvalue problem $\f (A - \lambda B) x = 0 $\f
-    /// \return Eigenvalues and vectors
+    /// \return eigenvalues and vectors
     std::pair<vector, matrix> solve(sparse_matrix const& A, sparse_matrix const& B);
 
 protected:
