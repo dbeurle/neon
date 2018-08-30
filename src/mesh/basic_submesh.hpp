d3cfe95c6f7d6943ad5bd9b6be675fe3df0f1d57
Darcy Beurle <darcy@beurle.id.au>
2018-08-30 13:50:07 +0200

    Use trailing return type

--- src/mesh/basic_submesh.hpp
+++ src/mesh/basic_submesh.hpp
@@ -22,16 +22,16 @@ public:
     auto nodes_per_element() const { return node_indices.rows(); }
 
     /// \return The element topology for this mesh
-    element_topology const& topology() const { return m_topology; }
+    auto topology() const -> element_topology { return m_topology; }
 
-    /// \return a list of the element nodal connectivities
-    index_view local_node_view(std::int64_t const element) const
+    /// \return a list of the element node indices
+    auto local_node_view(std::int64_t const element) const -> index_view
     {
         return node_indices(Eigen::placeholders::all, element);
     }
 
-    /// \return a list of unique nodal connectivities
-    std::vector<int32_t> unique_node_indices() const;
+    /// \return a list of unique node indices
+    auto unique_node_indices() const -> std::vector<int32_t>;
 
     /// \return a two dimensional array with element nodes
     auto const& all_node_indices() const { return node_indices; }
