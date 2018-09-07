fa870cf1fe7c7620d748e9a981f42e663014156b
Darcy Beurle <darcy@beurle.id.au>
2018-09-07 14:17:53 +0200

    Add basic test for finding compute devices

---
+++ test/compute_device.cpp
@@ -0,0 +1,30 @@
+
+#include <catch.hpp>
+
+#include "solver/find_compute_device.hpp"
+
+#include <iostream>
+
+#ifdef ENABLE_OCL
+
+#include <CL/cl.hpp>
+
+TEST_CASE("Find an OpenCL device")
+{
+    // Find a CPU device for testing on the build server
+    auto const device = neon::find_opencl_device();
+
+    std::cout << "A wild " << device.getInfo<CL_DEVICE_NAME>() << " appeared!\n";
+}
+
+#endif
+
+#ifdef ENABLE_CUDA
+
+TEST_CASE("Find a CUDA device")
+{
+    // Find a CPU device for testing on the build server
+    auto const device = neon::find_cuda_device();
+}
+
+#endif
