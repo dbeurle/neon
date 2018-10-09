8555792138dbeda232fc5a145134e9dc50cfce55
Darcy Beurle <darcy@beurle.id.au>
2018-10-09 08:04:11 +0200

    Include the correct header

--- src/interpolations/interpolation_factory.hpp
+++ src/interpolations/interpolation_factory.hpp
@@ -3,7 +3,7 @@
 
 #include "shape_function_forward.hpp"
 #include "mesh/element_topology.hpp"
-#include "math/bilinear_form.hpp"
+#include "math/weak_form.hpp"
 #include "io/json_forward.hpp"
 
 #include <memory>
@@ -15,11 +15,8 @@ namespace neon
 /// Factory method for the three dimensional shape functions
 /// \param topology Element topology
 /// \param simulation_data json object for element technology input options
-std::unique_ptr<volume_interpolation> make_volume_interpolation(
-    element_topology const topology,
-    json const& simulation_data,
-    std::variant<fem::linear, fem::bilinear> integral_form,
-    std::optional<fem::bilinear> bilinear_form);
+std::unique_ptr<volume_interpolation> make_volume_interpolation(element_topology const topology,
+                                                                json const& simulation_data);
 
 /// Factory method for the two dimensional shape functions
 /// \param topology Element topology
