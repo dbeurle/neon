f03ca48aca59898b8b3be23468be69d728c358d5
Darcy Beurle <darcy@beurle.id.au>
2018-11-13 12:56:38 +0100

    Use free function

--- src/material/isotropic_elastic_plastic.cpp
+++ src/material/isotropic_elastic_plastic.cpp
@@ -10,7 +10,7 @@ namespace neon
 isotropic_elastic_plastic::isotropic_elastic_plastic(json const& material_data)
     : isotropic_elastic_property(material_data)
 {
-    if (material_data.find("yield_stress") == material_data.end())
+    if (material_data.find("yield_stress") == end(material_data))
     {
         throw std::domain_error("\"yield_stress\" was not specified in material properties");
     }
@@ -18,11 +18,11 @@ isotropic_elastic_plastic::isotropic_elastic_plastic(json const& material_data)
     stress_y = material_data["yield_stress"];
 
     // Optional material parameters
-    if (material_data.find("isotropic_hardening_modulus") != material_data.end())
+    if (material_data.find("isotropic_hardening_modulus") != end(material_data))
     {
         H_iso = material_data["isotropic_hardening_modulus"];
     }
-    if (material_data.find("isotropic_kinematic_modulus") != material_data.end())
+    if (material_data.find("isotropic_kinematic_modulus") != end(material_data))
     {
         K_iso = material_data["isotropic_kinematic_modulus"];
     }
