27f3f6256dd3c734ea6e83b675fdcd671cbe3135
Darcy Beurle <darcy@beurle.id.au>
2018-11-13 12:39:45 +0100

    Use find instead of count

--- src/material/isotropic_elastic_plastic_damage.cpp
+++ src/material/isotropic_elastic_plastic_damage.cpp
@@ -10,27 +10,27 @@ namespace neon
 isotropic_elastic_plastic_damage::isotropic_elastic_plastic_damage(json const& material_data)
     : isotropic_elastic_plastic(material_data)
 {
-    if (!material_data.count("softening_multiplier"))
+    if (material_data.find("softening_multiplier") == end(material_data))
     {
         throw std::domain_error("\"softening_multiplier\" needs to be specified");
     }
-    if (!material_data.count("kinematic_hardening_modulus"))
+    if (material_data.find("kinematic_hardening_modulus") == end(material_data))
     {
         throw std::domain_error("\"kinematic_hardening_modulus\" needs to be specified");
     }
-    if (!material_data.count("plasticity_viscous_exponent"))
+    if (material_data.find("plasticity_viscous_exponent") == end(material_data))
     {
         throw std::domain_error("\"plasticity_viscous_exponent\" needs to be specified");
     }
-    if (!material_data.count("plasticity_viscous_denominator"))
+    if (material_data.find("plasticity_viscous_denominator") == end(material_data))
     {
         throw std::domain_error("\"plasticity_viscous_denominator\" needs to be specified");
     }
-    if (!material_data.count("damage_viscous_exponent"))
+    if (material_data.find("damage_viscous_exponent") == end(material_data))
     {
         throw std::domain_error("\"damage_viscous_exponent\" needs to be specified");
     }
-    if (!material_data.count("damage_viscous_denominator"))
+    if (material_data.find("damage_viscous_denominator") == end(material_data))
     {
         throw std::domain_error("\"damage_viscous_denominator\" needs to be specified");
     }
