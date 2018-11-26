3213ccff0f6d4d3f141c14d12fa17c5d05eafc4b
Darcy Beurle <darcy@beurle.id.au>
2018-11-26 09:07:49 +0100

    Check correct eigenvalue

--- test/eigenvalue_solvers.cpp
+++ test/eigenvalue_solvers.cpp
@@ -48,7 +48,7 @@ TEST_CASE("Arpack eigenvalues")
     for (int i = 0; i < 10; i++)
     {
         // Expected eigenvalues
-        REQUIRE(values(i) == Approx(41.0 + i));
+        REQUIRE(values(i) == Approx(i + 1.0));
         // Unit vectors
         REQUIRE(vectors.col(i).norm() == Approx(1.0));
     }
