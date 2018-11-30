a90f27e068646b57db2392ef28bdee5d11c7e3cb
Darcy Beurle <darcy@beurle.id.au>
2018-11-30 18:47:41 +0100

    Add sixteen to available schemes

--- src/quadrature/tetrahedron_quadrature.hpp
+++ src/quadrature/tetrahedron_quadrature.hpp
@@ -9,7 +9,7 @@ class tetrahedron_quadrature : public volume_quadrature
 {
 public:
     /// Available quadrature rules for this element type
-    enum class point { one, four, five };
+    enum class point { one, four, five, sixteen };
 
     explicit tetrahedron_quadrature(point const p);
 };
