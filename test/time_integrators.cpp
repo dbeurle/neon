674a429db3cb20c9d13ebf2472d084ff0fb010cf
Darcy Beurle <darcy@beurle.id.au>
2018-12-22 09:16:38 +0100

    Remove unused parameters

--- test/time_integrators.cpp
+++ test/time_integrators.cpp
@@ -24,31 +24,24 @@ TEST_CASE("interval")
 
 TEST_CASE("dy/dt = 1 integration")
 {
-    REQUIRE(explicit_euler(0.0, 10.0, 0.0, [](auto const t, double const y) { return 1.0; })
-            == Approx(10.0));
+    REQUIRE(explicit_euler(0.0, 10.0, 0.0, [](auto, auto) { return 1.0; }) == Approx(10.0));
 
-    REQUIRE(runge_kutta_second_order(0.0, 10.0, 0.0, [](auto const t, double const y) { return 1.0; })
-            == Approx(10.0));
+    REQUIRE(runge_kutta_second_order(0.0, 10.0, 0.0, [](auto, auto) { return 1.0; }) == Approx(10.0));
 
-    REQUIRE(runge_kutta_fourth_order(0.0, 10.0, 0.0, [](auto const t, auto const y) { return 1.0; })
-            == Approx(10.0));
+    REQUIRE(runge_kutta_fourth_order(0.0, 10.0, 0.0, [](auto, auto) { return 1.0; }) == Approx(10.0));
 }
 TEST_CASE("dy/dt = y integration")
 {
     // Exact solution should be std::exp(1.0)
-    REQUIRE(explicit_euler(0.0, 1.0, 1.0, [](double const t, double const y) { return y; })
-            == Approx(2.0));
+    REQUIRE(explicit_euler(0.0, 1.0, 1.0, [](double, double const y) { return y; }) == Approx(2.0));
 
-    REQUIRE(runge_kutta_second_order(0.0, 1.0, 1.0, [](double const t, double const y) { return y; })
+    REQUIRE(runge_kutta_second_order(0.0, 1.0, 1.0, [](double, double const y) { return y; })
             == Approx(2.5));
 
-    REQUIRE(runge_kutta_fourth_order(0.0, 1.0, 1.0, [](double const t, double const y) { return y; })
+    REQUIRE(runge_kutta_fourth_order(0.0, 1.0, 1.0, [](double, double const y) { return y; })
             == Approx(2.70833));
 
-    REQUIRE(runge_kutta_fourth_fifth_order(0.0,
-                                           1.0,
-                                           1.0,
-                                           [](double const t, double const y) { return y; })
+    REQUIRE(runge_kutta_fourth_fifth_order(0.0, 1.0, 1.0, [](double, double const y) { return y; })
             == Approx(std::exp(1.0)));
 }
 TEST_CASE("dy/dt = y integration with system")
@@ -57,7 +50,7 @@ TEST_CASE("dy/dt = y integration with system")
     REQUIRE((runge_kutta_fourth_fifth_order(0.0,
                                             1.0,
                                             vector4::Ones().eval(),
-                                            [](double const t, vector4 y) { return y; })
+                                            [](double, vector4 y) { return y; })
              - std::exp(1.0) * vector4::Ones())
                 .norm()
             == Approx(0.0).margin(ZERO_MARGIN));
